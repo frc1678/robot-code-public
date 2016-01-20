888d149d1e15daa00bcdf8953e9452df47ced02e
Kyle Stachowicz <kylestach99@gmail.com>
2016-01-19 21:42:00 -0800

    Separated timing utils into a .h and a .cpp file to resolve multiple-definition errors

--- utils/timing_utils.h
+++ utils/timing_utils.h
@@ -8,22 +8,9 @@
 
 namespace muan {
 
-void sleep_for(Time t) {
-  std::this_thread::sleep_for(std::chrono::milliseconds((int)t.to(ms)));
-}
-
-Time now() {
-  using namespace std::chrono;
-  auto since_epoch = system_clock::now().time_since_epoch();
-  return duration_cast<microseconds>(since_epoch).count() * us;
-}
-
-void sleep_until(Time t) {
-  using namespace std::chrono;
-  auto as_time_point =
-      time_point<system_clock>(microseconds(static_cast<long long>(t.to(us))));
-  std::this_thread::sleep_until(as_time_point);
-}
+void sleep_for(Time t);
+Time now();
+void sleep_until(Time t);
 }
 
 #endif  // MUAN_UTILS_TIMING_UTILS_H_
