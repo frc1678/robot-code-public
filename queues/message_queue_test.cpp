ba6407af32eadb7c601be5092bfd9db9d9a73920
Kyle Stachowicz <kylestach99@gmail.com>
2016-07-31 18:33:50 -0700

    Improve message queue tests and comment tests appropriately
    In the message queue tests, ensure that the messages are being received
    in order (likely not a possible bug with this particular implementation,
    but a good one to check for nonetheless).
    
    Also change ASSERT_* to EXPECT_*.

--- queues/message_queue_test.cpp
+++ queues/message_queue_test.cpp
@@ -4,97 +4,114 @@
 
 using namespace muan::queues;
 
+// Ensure that the queue delivers a single message correctly
 TEST(MessageQueue, DeliversSingleMessage) {
   MessageQueue<uint32_t, 10> int_queue;
   int_queue.WriteMessage(10);
   auto reader = int_queue.MakeReader();
-  ASSERT_EQ(reader.ReadMessage().value(), 10);
+  EXPECT_EQ(reader.ReadMessage().value(), 10);
 }
 
+// Ensure that the queue delivers multiple messages correctly and in sequence
 TEST(MessageQueue, DeliversManyMessages) {
   MessageQueue<uint32_t, 10> int_queue;
   auto reader = int_queue.MakeReader();
-  for (uint32_t i = 1; i <= 10; i++) {
+  for (uint32_t i = 0; i < 10; i++) {
     int_queue.WriteMessage(i);
   }
-  uint64_t accum = 0;
+
+  uint32_t next = 0;
   std::experimental::optional<uint32_t> val;
   while (val = reader.ReadMessage()) {
-    accum++;
+    EXPECT_EQ(*val, next);
+    next++;
   }
-  ASSERT_EQ(accum, 10);
+  EXPECT_EQ(next, 10);
 }
 
+// Ensure that correctness is maintained when the queue is filled and the
+// message positions wrap around to the beginning of the buffer
 TEST(MessageQueue, Wraparound) {
   MessageQueue<uint32_t, 10> int_queue;
   auto reader = int_queue.MakeReader();
-  for (uint32_t i = 1; i <= 10; i++) {
+  for (uint32_t i = 0; i < 10; i++) {
     int_queue.WriteMessage(i);
   }
-  uint64_t accum = 0;
+
+  uint64_t next = 0;
   std::experimental::optional<uint32_t> val;
   while (val = reader.ReadMessage()) {
-    accum++;
+    EXPECT_EQ(*val, next);
+    next++;
   }
 
-  for (uint32_t i = 11; i <= 15; i++) {
+  for (uint32_t i = 10; i < 15; i++) {
     int_queue.WriteMessage(i);
   }
+
   while (val = reader.ReadMessage()) {
-    accum++;
+    EXPECT_EQ(*val, next);
+    next++;
   }
-  ASSERT_EQ(accum, 15);
+
+  EXPECT_EQ(next, 15);
 }
 
+// Ensure that the queue still works with multiple readers
 TEST(MessageQueue, TwoReaders) {
   MessageQueue<uint32_t, 10> int_queue;
+
   auto reader1 = int_queue.MakeReader();
   auto reader2 = int_queue.MakeReader();
-  for (uint32_t i = 1; i <= 10; i++) {
+
+  for (uint32_t i = 0; i < 10; i++) {
     int_queue.WriteMessage(i);
   }
 
-  uint64_t accum1 = 0;
+  uint64_t next1 = 0;
   std::experimental::optional<uint32_t> val;
   while (val = reader1.ReadMessage()) {
-    accum1++;
+    EXPECT_EQ(*val, next1);
+    next1++;
   }
 
-  uint64_t accum2 = 0;
+  uint64_t next2 = 0;
   while (val = reader2.ReadMessage()) {
-    accum2++;
+    EXPECT_EQ(*val, next2);
+    next2++;
   }
-  ASSERT_EQ(accum1, 10);
-  ASSERT_EQ(accum2, 10);
+  EXPECT_EQ(next1, 10);
+  EXPECT_EQ(next2, 10);
 }
 
+// Test the queue's speed with large values. This test is mainly for curiosity's
+// sake and will not fail if the queue fails to acheive some arbitrarily set
+// standard
 TEST(MessageQueue, SpeedTest) {
   MessageQueue<uint32_t, 1000000> int_queue;
-  auto reader1 = int_queue.MakeReader();
-  auto reader2 = int_queue.MakeReader();
-  for (uint32_t i = 1; i <= 1000000; i++) {
+  auto reader = int_queue.MakeReader();
+
+  for (uint32_t i = 0; i < 1000000; i++) {
     int_queue.WriteMessage(i);
   }
 
-  uint64_t accum1 = 0;
+  uint64_t next = 0;
   std::experimental::optional<uint32_t> val;
-  while (val = reader1.ReadMessage()) {
-    accum1++;
+  while (val = reader.ReadMessage()) {
+    EXPECT_EQ(*val, next);
+    next++;
   }
 
-  uint64_t accum2 = 0;
-  while (val = reader2.ReadMessage()) {
-    accum2++;
-  }
-  ASSERT_EQ(accum1, 1000000);
-  ASSERT_EQ(accum2, 1000000);
+  EXPECT_EQ(next, 1000000);
 }
 
+// Ensure that the queues maintain correctness when being accessed by many
+// reader threads and a single writer thread
 TEST(MessageQueue, Multithreading) {
   constexpr uint32_t num_messages = 10000;
   MessageQueue<uint32_t, num_messages> int_queue;
   auto func = [&int_queue]() {
-    uint32_t count = 0;
+    uint32_t next = 0;
     auto reader = int_queue.MakeReader();
     auto end_time =
         std::chrono::steady_clock::now() + std::chrono::milliseconds(300);
@@ -103,11 +120,11 @@ TEST(MessageQueue, Multithreading) {
     while (std::chrono::steady_clock::now() < end_time) {
       auto val = reader.ReadMessage();
       if (val) {
-        EXPECT_EQ(count, *val);
-        count++;
+        EXPECT_EQ(next, *val);
+        next++;
       }
     }
-    EXPECT_EQ(count, num_messages);
+    EXPECT_EQ(next, num_messages);
   };
 
   std::array<std::thread, 5> threads;
