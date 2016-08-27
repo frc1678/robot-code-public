ff96230a7a1f1fadaa40d5e2efad81016015caf7
Finn Boire <finnegan.mccool@gmail.com>
2016-08-26 18:39:20 -0700

    Add basic units

---
+++ muan/units/units.h
@@ -0,0 +1,42 @@
+namespace muan {
+
+namespace units {
+
+// Unitless
+constexpr double pi = 3.14159265358;
+
+// Time
+constexpr double s = 1 * s;
+constexpr double min = 60 * s;
+constexpr double ms = 1e-3 * s;
+constexpr double us = 1e-6 * s;
+
+// Lengths
+constexpr double m = 1;
+constexpr double cm = 1e-2 * m;
+constexpr double in = 2.54 * cm;
+constexpr double ft = 12 * in;
+
+// Mass
+constexpr double kg = 1;
+constexpr double g = 0.001 * kg;
+constexpr double mg = 0.001 * g;
+
+// Angle
+constexpr double rad = 1;
+constexpr double deg = 1.74532925e-2 * rad;
+constexpr double rev = 2 * pi * rad;
+
+// Angular Velocity
+constexpr double rpm = rev / min;
+
+// Current
+constexpr double A = 1;
+
+// Voltage
+constexpr double V = kg * m * m / (A * s * s * s);
+constexpr double pwm = 12 * V;
+
+}  // units
+
+}  // muan
