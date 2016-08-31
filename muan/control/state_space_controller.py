73f336ab47c8d0a6b9702c2584c15d56774e631c
Kyle Stachowicz <kylestach99@gmail.com>
2016-08-30 19:09:10 -0700

    Add gain-scheduling to python controls

--- muan/control/state_space_controller.py
+++ muan/control/state_space_controller.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 import numpy as np
-import state_space_plant
 import controls
 import matplotlib.pyplot as plt
 
@@ -11,30 +10,35 @@ A state-space controller class with support for feedforward control.
 __author__ = 'Kyle Stachowicz (kylestach99@gmail.com)'
 
 class StateSpaceController(object):
-    def __init__(self, K, Kff, plant, u_min = None, u_max = None):
-        if u_min is None:
-            u_min = np.full(plant.x.shape, -np.inf)
-        if u_max is None:
-            u_max = np.full(plant.x.shape, np.inf)
+    def __init__(self, gains, u_min, u_max):
+        self.u_min = np.asmatrix(u_min or np.full(plant.x.shape, -np.inf))
+        self.u_max = np.asmatrix(u_max or -u_min)
 
-        self.u_min = np.asmatrix(u_min)
-        self.u_max = np.asmatrix(u_max)
-        self.K = np.asmatrix(K)
-        self.Kff = np.asmatrix(Kff)
-        self.sys = plant
+        if not isinstance(gains, list):
+            gains = [gains]
+        self.gains = gains
+        self.current_gains = 0
 
-        self.r = np.asmatrix(np.zeros(K.shape[1]))
+        assert len(self.gains) > 0, "Must have at least one set of gains."
+
+        self.r = np.asmatrix(np.zeros(self.gains[0].B.shape[1]))
+
+    def set_gains(self, gains):
+        assert gains < len(self.gains), "Gains id must be in range."
+        self.current_gains = gains
 
     def update(self, x, goal_next = None):
+        gains = self.gains[self.current_gains]
+
         # To use a moving goal, pass in a value for goal_next
         if goal_next is None:
             goal_next = self.r
 
         # u from the feed-forwards term
-        uff = self.Kff * (goal_next - self.sys.A_d * self.r)
+        uff = gains.Kff * (goal_next - gains.A * self.r)
 
         # u from the closed-loop controller
-        uc = self.K * (self.r - x)
+        uc = gains.K * (self.r - x)
 
         self.r = goal_next
 
@@ -42,24 +46,3 @@ class StateSpaceController(object):
         u = np.clip(u, self.u_min, self.u_max)
 
         return u
-
-"""
-The formula for Kff can be derived as follows:
-    r(n+1) = A*r(n) + B*u_ff
-    B*u_ff = r(n+1) - A*r(n)
-    u_ff = pinv(B)*(r(n+1) - A*r(n))
-    Kff = B
-    u_ff = Kff*(r(n+1) - A*r(n))
-There is also an LQR-weighted solution, but it gives the same u_ff assuming
-that there is some u_ff that satisfies the equation above.
-"""
-
-def placement(plant, poles, u_min = None, u_max = None):
-    K = controls.place(plant.A_d, plant.B_d, poles)
-    Kff = np.linalg.pinv(plant.B_d)
-    return StateSpaceController(K, Kff, plant, u_min, u_max)
-
-def lqr(plant, Q, R, u_min = None, u_max = None):
-    K = controls.dlqr(plant.A_d, plant.B_d, Q, R)
-    Kff = np.linalg.inv(plant.B_d.T * Q * plant.B_d) * plant.B_d.T * Q.T
-    return StateSpaceController(K, Kff, plant, u_min, u_max)
--- muan/control/state_space_scenario.py
+++ muan/control/state_space_scenario.py
@@ -2,9 +2,6 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 from functools import reduce
-import state_space_plant
-import state_space_controller
-import state_space_observer
 from copy import copy
 
 """
@@ -65,7 +62,7 @@ class StateSpaceScenario(object):
 
     def run(self, goal, total_time):
         self.reset()
-        self.t_history = np.linspace(0, total_time, total_time / self.sys.dt + 1)
+        self.t_history = np.linspace(0, total_time, total_time / self.sys.get_current_gains().dt + 1)
 
         self.controller.r = goal(0)
 
@@ -79,7 +76,7 @@ class StateSpaceScenario(object):
             # Order: Get u(n+1) from the controller
             #        Use u(n+1) and y(n) = Cx(n) for the observer
             #        Use u(n+1) to update the actual system
-            u = self.controller.update(self.observer.x_hat, goal(t + self.sys.dt))
+            u = self.controller.update(self.observer.x_hat, goal(t + self.sys.get_current_gains().dt))
             self.observer.update(self.sys.y, u)
             self.sys.update(u)
 
@@ -134,29 +131,15 @@ class StateSpaceScenario(object):
         return '\n'.join(['}} /* {} */'.format(ns) for ns in self.namespaces])
 
 
-    # Get an array of all matrices to write to the C++ file
-    def _writable_matrices(self):
-        return [
-            ('ContinuousA', self.sys.A_c),
-            ('A', self.sys.A_d),
-            ('ContinuousB', self.sys.B_c),
-            ('B', self.sys.B_d),
-            ('C', self.sys.C),
-            ('D', self.sys.D),
-            ('K', self.controller.K),
-            ('Kff', self.controller.Kff),
-            ('L', self.observer.L),
-        ]
-
     # Write the declaration for a single C++ matrix generator
     def _matrix_generator_declaration(self, name, mat):
         format_string = 'Eigen::Matrix<double, {n}, {m}> {name}();'
         return format_string.format(n = mat.shape[0], m = mat.shape[1], name = name)
 
     # Write all C++ matrix generator declarations
-    def _matrix_generator_declarations(self):
+    def _matrix_generator_declarations(self, writable_matrices):
         return '\n'.join(
-            [self._matrix_generator_declaration(name, mat) for name, mat in self._writable_matrices()]
+            [self._matrix_generator_declaration(name, writable_matrices[name]) for name in writable_matrices]
         )
 
     # Write a single C++ matrix generator
@@ -169,10 +152,30 @@ class StateSpaceScenario(object):
         return format_string.format(n = mat.shape[0], m = mat.shape[1], name = name, entries = entries)
 
     # Write all C++ matrix generators
-    def _matrix_generator_definitions(self):
+    def _matrix_generator_definitions(self, writable_matrices):
         return '\n'.join(
-            [self._matrix_generator_definition(name, mat) for name, mat in self._writable_matrices()]
+            [self._matrix_generator_definition(name, writable_matrices[name]) for name in writable_matrices]
         )
+
+    def _gain_generator_declarations(self):
+        out_string = ''
+        for gain in self.sys.gains:
+            out_string += 'namespace %s {' % gain.name
+            out_string += self._matrix_generator_declarations(gain.writable_matrices())
+            out_string += '} // namespace %s' % gain.name
+        return out_string
+
+    def _gain_generator_definitions(self):
+        out_string = ''
+        for gain in self.sys.gains:
+            if len(self.sys.gains) == 1:
+                out_string += 'namespace %s {\n' % gain.name
+
+            out_string += self._matrix_generator_definitions(gain.writable_matrices())
+
+            if len(self.sys.gains) == 1:
+                out_string += '\n} // namespace %s' % gain.name
+        return out_string
 
     # Write C++ code to a header file and a cpp file
     def write(self, h_file_name, cpp_file_name):
@@ -182,13 +185,13 @@ class StateSpaceScenario(object):
 #include "Eigen/Core"
 
 {namespace_openers}
-{matrix_generators}
+{gain_generators}
 {namespace_closers}
-'''.format(namespace_openers = self._namespace_openers(), matrix_generators = self._matrix_generator_declarations(), namespace_closers = self._namespace_closers()))
+'''.format(namespace_openers = self._namespace_openers(), gain_generators = self._gain_generator_declarations(), namespace_closers = self._namespace_closers()))
 
             cpp_file.write(
 '''#include "{header_name}"
 {namespace_openers}
-{matrix_generators}
+{gain_generators}
 {namespace_closers}
-'''.format(header_name = h_file_name, namespace_openers = self._namespace_openers(), matrix_generators = self._matrix_generator_definitions(), namespace_closers = self._namespace_closers()))
+'''.format(header_name = h_file_name, namespace_openers = self._namespace_openers(), gain_generators = self._gain_generator_definitions(), namespace_closers = self._namespace_closers()))
