9a706dd13942ba3eb0c647bf7bf101cff0c2d0ea
Kyle Stachowicz <kylestach99@gmail.com>
2016-09-04 16:40:41 -0700

    Set up skeletons for drivetrain_subsystem

---
+++ o2016/subsystems/drivetrain/drivetrain_subsystem.cpp
@@ -0,0 +1,7 @@
+#include "drivetrain_subsystem.h"
+
+namespace frc1678 {
+
+namespace drivetrain {} /* drivetrain */
+
+} /* frc1678 */
---
+++ o2016/subsystems/drivetrain/drivetrain_subsystem.h
@@ -0,0 +1,12 @@
+#ifndef O2016_SUBSYSTEMS_DRIVETRAIN_DRIVETRAIN_SUBSYSTEM_H_
+#define O2016_SUBSYSTEMS_DRIVETRAIN_DRIVETRAIN_SUBSYSTEM_H_
+
+#include "o2016/subsystems/drivetrain/drivetrain_constants.h"
+
+namespace frc1678 {
+
+namespace drivetrain {} /* drivetrain */
+
+} /* frc1678 */
+
+#endif /* ifndef O2016_SUBSYSTEMS_DRIVETRAIN_DRIVETRAIN_SUBSYSTEM_H_ */
