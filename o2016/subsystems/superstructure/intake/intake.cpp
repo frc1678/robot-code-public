1f45cd5f212ac4ec3b0c0463589c35102344ab1c
= <=>
2016-10-19 21:01:33 -0700

    Created the build file and added basic structure to the cpp file

--- o2016/subsystems/superstructure/intake/intake.cpp
+++ o2016/subsystems/superstructure/intake/intake.cpp
@@ -1,7 +1,42 @@
 #include "intake.h"
 
-namespace frc1678 {
+using namespace muan::control;
+using namespace muan::units;
 
-namespace intake{}
+namespace frc1678{
 
-} /* frc1678 */
+namespace o2016 {
+
+namespace intake {
+
+Intake::Intake() {
+  using namespace frc1678::intake_controller;
+  auto ss_plant = StateSpacePlant<1, 2, 1> (controller::A(), controller::B(), controller::C());
+  controller_ = StateSpaceController<1, 2, 1> (controller::K());
+  controller_.u_min() = Eigen::Matrix<double, 1, 1>::Ones() * -12.0;
+  controller_.u_max() = Eigen::Matrix<double, 1, 1>::Ones() * 12.0;
+  observer_ = StateSpaceObserver<1, 2, 1> (ss_plant, controller::L());
+  done = false;
+}
+
+Voltage Intake::Update(Angle goal, Angle sensor_value) {
+  
+}
+
+Angle Intake::GetAngle() const {
+  
+}
+
+void Intake::SetAngle(Angle theta) {
+  //observer_.x()(0,0) = theta;
+}
+
+bool Intake::IsDone() const {
+  return done;
+}
+
+}
+
+}
+
+}
--- o2016/subsystems/superstructure/intake/intake.h
+++ o2016/subsystems/superstructure/intake/intake.h
@@ -1,11 +1,37 @@
 #ifndef O2016_SUBSYSTEMS_SUPERSTRUCTURE_INTAKE_H_
 #define O0216_SUBSYSTEMS_SUPERSTRUCTURE_INTAKE_H_
 
-#include "o2016/subsystems/superstructure/intake_constants.h"
+#include "o2016/subsystems/superstructure/intake/intake_constants.h"
+#include "muan/units/units.h"
+#include "muan/control/state_space_plant.h"
+#include "muan/control/state_space_controller.h"
+#include "muan/control/state_space_observer.h"
 
-namespace frc1678 {
+namespace frc1678{
 
-namespace intake {}
+namespace o2016 {
+
+namespace intake {
+
+class Intake {
+  public: 
+    Intake();
+    ~Intake();
+    muan::units::Voltage Update(muan::units::Angle goal, muan::units::Angle sensor_value);
+    muan::units::Angle GetAngle() const;
+    void SetAngle(muan::units::Angle theta);
+    bool IsDone() const;
+
+  private:
+    muan::control::StateSpaceController<1, 2, 1> controller_;
+    muan::control::StateSpaceObserver<1, 2, 1> observer_;
+    bool done;
+
+};
+
+}
+
+}
 
 }
 
