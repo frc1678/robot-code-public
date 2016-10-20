d9bf05cb71aa0ba001078a59e1c6fd2e1b7ec3e7
Kyle Stachowicz <kylestach99@gmail.com>
2016-10-19 20:02:42 -0700

    Add PCM wrapper

---
+++ muan/wpilib/pcm_wrapper.cpp
@@ -0,0 +1,53 @@
+#include "pcm_wrapper.h"
+#include "third_party/aos/common/die.h"
+
+namespace muan {
+
+namespace wpilib {
+
+void PcmWrapper::CreateSolenoid(uint8_t port) {
+  // Mark this channel as initialized
+  initialized_ |= (1 << port);
+}
+
+void PcmWrapper::WriteDoubleSolenoid(uint8_t channel_forward,
+                                     uint8_t channel_reverse,
+                                     DoubleSolenoid::Value value) {
+  SetChannel(channel_forward, value == DoubleSolenoid::Value::kForward);
+  SetChannel(channel_reverse, value == DoubleSolenoid::Value::kReverse);
+}
+
+void PcmWrapper::WriteSolenoid(uint8_t channel, bool on) {
+  SetChannel(channel, on);
+}
+
+void PcmWrapper::Flush() {
+  // Write the cached values to CAN
+  SolenoidBase::Set(current_values_, 0xFF, m_moduleNumber);
+}
+
+void PcmWrapper::CheckPortInitialized(uint8_t channel) {
+  if (!(initialized_ & (1 << channel))) {
+    aos::Die("Solenoid port %i not initialized!", channel);
+  }
+}
+
+void PcmWrapper::SetChannel(uint8_t channel, bool on) {
+  // Before we do anything, check that the port is initialized. This isn't
+  // really necessary, as WPILib automatically initializes all of the channels
+  // on the PCM (as far as I can tell), but it is a nice check to make sure
+  // people only write to solenoids that they actually mean to use.
+  CheckPortInitialized(channel);
+
+  if (on) {
+    // Set the bit
+    current_values_ |= (1 << channel);
+  } else {
+    // Clear the bit
+    current_values_ &= ~(1 << channel);
+  }
+}
+
+}  // wpilib
+
+}  // muan
---
+++ muan/wpilib/pcm_wrapper.h
@@ -0,0 +1,47 @@
+#ifndef MUAN_WPILIB_PCM_WRAPPER_H_
+#define MUAN_WPILIB_PCM_WRAPPER_H_
+
+#include "WPILib.h"
+#include <atomic>
+#include <cstdint>
+
+namespace muan {
+
+namespace wpilib {
+
+class PcmWrapper : private SolenoidBase {
+ public:
+  PcmWrapper(uint8_t module = 0) : SolenoidBase(module) {}
+  ~PcmWrapper() = default;
+
+  // Create a solenoid on the specified channel.
+  void CreateSolenoid(uint8_t port);
+  void CreateDoubleSolenoid(uint8_t channel_forward, uint8_t channel_reverse);
+
+  // Writes to the specified solenoid port. This is realtime and just sets
+  // values that will be written in the CAN thread. These functions will die if
+  // the corresponding channel is not initialized.
+  void WriteSolenoid(uint8_t channel, bool on);
+  void WriteDoubleSolenoid(uint8_t channel_forward, uint8_t channel_reverse,
+                           DoubleSolenoid::Value value);
+
+  // Flushes the current values to the CAN bus. This function is not realtime
+  // and should only be called from the CAN thread.
+  void Flush();
+
+ private:
+  // Make sure the port is initialized, and die if it is not
+  void CheckPortInitialized(uint8_t port);
+
+  void SetChannel(uint8_t channel, bool on);
+
+  // Bitmasks of the initialized channels as well as the current values of each
+  // solenoid
+  std::atomic<uint8_t> initialized_, current_values_;
+};
+
+}  // wpilib
+
+}  // muan
+
+#endif  // MUAN_WPILIB_PCM_WRAPPER_H_
