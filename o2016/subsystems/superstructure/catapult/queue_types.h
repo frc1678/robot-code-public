bc3086a317a595e17b36f0e34827415c6de86e1f
Lucas Solomon <amannababanana@gmail.com>
2016-11-01 22:25:42 -0700

    Increase memory allocated to catapult protobufs; add namespaces

--- o2016/subsystems/superstructure/catapult/queue_types.h
+++ o2016/subsystems/superstructure/catapult/queue_types.h
@@ -5,14 +5,22 @@
 #include "muan/queues/message_queue.h"
 #include "o2016/subsystems/superstructure/catapult/catapult.pb.h"
 
-using CatapultOutputProto = muan::proto::StackProto<CatapultOutput, 100>;
-using CatapultStatusProto = muan::proto::StackProto<CatapultStatus, 100>;
-using CatapultInputProto = muan::proto::StackProto<CatapultInput, 100>;
-using CatapultGoalProto = muan::proto::StackProto<CatapultGoal, 100>;
+namespace o2016 {
+
+namespace catapult{
+
+using CatapultOutputProto = muan::proto::StackProto<CatapultOutput, 1024>;
+using CatapultStatusProto = muan::proto::StackProto<CatapultStatus, 1024>;
+using CatapultInputProto = muan::proto::StackProto<CatapultInput, 1024>;
+using CatapultGoalProto = muan::proto::StackProto<CatapultGoal, 1024>;
 
 using CatapultOutputQueue = muan::queues::MessageQueue<CatapultOutputProto, 100>;
 using CatapultStatusQueue = muan::queues::MessageQueue<CatapultStatusProto, 100>;
 using CatapultInputQueue = muan::queues::MessageQueue<CatapultInputProto, 100>;
 using CatapultGoalQueue = muan::queues::MessageQueue<CatapultGoalProto, 100>;
+
+} // catapult
+
+} // o2016
 
 #endif // O2016_SUBSYSTEMS_SUPERSTRUCTURE_CATAPULT_QUEUETYPES_H_
