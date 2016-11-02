75f4ac4f7a0c451991f987606bd7fd37dece5fc0
Kyle Stachowicz <kylestach99@gmail.com>
2016-11-02 16:05:36 -0700

    Add new queues to QueueManager

--- o2016/queue_manager/queue_manager.cpp
+++ o2016/queue_manager/queue_manager.cpp
@@ -7,8 +7,76 @@ QueueManager& QueueManager::GetInstance() {
   return instance;
 }
 
-MessageQueue<muan::proto::StackProto<PdpStatus, 512>>& QueueManager::pdp_status_queue() {
+MessageQueue<muan::proto::StackProto<PdpStatus, 512>>&
+QueueManager::pdp_status_queue() {
   return pdp_status_queue_;
 }
 
+o2016::turret::TurretInputQueue& QueueManager::turret_input_queue() {
+  return turret_input_queue_;
 }
+
+o2016::turret::TurretGoalQueue& QueueManager::turret_goal_queue() {
+  return turret_goal_queue_;
+}
+
+o2016::turret::TurretStatusQueue& QueueManager::turret_status_queue() {
+  return turret_status_queue_;
+}
+
+o2016::turret::TurretOutputQueue& QueueManager::turret_output_queue() {
+  return turret_output_queue_;
+}
+
+o2016::drivetrain::DrivetrainInputQueue&
+QueueManager::drivetrain_input_queue() {
+  return drivetrain_input_queue_;
+}
+
+o2016::drivetrain::DrivetrainGoalQueue& QueueManager::drivetrain_goal_queue() {
+  return drivetrain_goal_queue_;
+}
+
+o2016::drivetrain::DrivetrainStatusQueue&
+QueueManager::drivetrain_status_queue() {
+  return drivetrain_status_queue_;
+}
+
+o2016::drivetrain::DrivetrainOutputQueue&
+QueueManager::drivetrain_output_queue() {
+  return drivetrain_output_queue_;
+}
+
+o2016::catapult::CatapultInputQueue& QueueManager::catapult_input_queue() {
+  return catapult_input_queue_;
+}
+
+o2016::catapult::CatapultGoalQueue& QueueManager::catapult_goal_queue() {
+  return catapult_goal_queue_;
+}
+
+o2016::catapult::CatapultStatusQueue& QueueManager::catapult_status_queue() {
+  return catapult_status_queue_;
+}
+
+o2016::catapult::CatapultOutputQueue& QueueManager::catapult_output_queue() {
+  return catapult_output_queue_;
+}
+
+o2016::intake::IntakeInputQueue& QueueManager::intake_input_queue() {
+  return intake_input_queue_;
+}
+
+o2016::intake::IntakeGoalQueue& QueueManager::intake_goal_queue() {
+  return intake_goal_queue_;
+}
+
+o2016::intake::IntakeStatusQueue& QueueManager::intake_status_queue() {
+  return intake_status_queue_;
+}
+
+o2016::intake::IntakeOutputQueue& QueueManager::intake_output_queue() {
+  return intake_output_queue_;
+}
+
+}  // o2016
--- o2016/queue_manager/queue_manager.h
+++ o2016/queue_manager/queue_manager.h
@@ -6,6 +6,11 @@
 
 #include "muan/wpilib/state.pb.h"
 
+#include "o2016/subsystems/drivetrain/queue_types.h"
+#include "o2016/subsystems/superstructure/catapult/queue_types.h"
+#include "o2016/subsystems/superstructure/intake/queue_types.h"
+#include "o2016/subsystems/superstructure/turret/queue_types.h"
+
 using muan::queues::MessageQueue;
 
 namespace o2016 {
@@ -15,21 +20,61 @@ namespace o2016 {
 // (because that would be gross). Instead, we can just have an instance of this
 // as a global, which is much less sketchy.
 class QueueManager {
-  public:
-    static QueueManager& GetInstance();
+ public:
+  static QueueManager& GetInstance();
 
-    // Note: This needs to be the same as the actual message queue in the
-    // PdpWrapper class. If you change that, you will need to change this.
-    // It is like this to avoid making QueueManager rely on WPILib.
-    MessageQueue<muan::proto::StackProto<PdpStatus, 512>>& pdp_status_queue();
+  // Note: This needs to be the same as the actual message queue in the
+  // PdpWrapper class. If you change that, you will need to change this.
+  // It is like this to avoid making QueueManager rely on WPILib.
+  MessageQueue<muan::proto::StackProto<PdpStatus, 512>>& pdp_status_queue();
 
-  private:
-    QueueManager() = default;
-    ~QueueManager() = default;
+  o2016::turret::TurretInputQueue& turret_input_queue();
+  o2016::turret::TurretGoalQueue& turret_goal_queue();
+  o2016::turret::TurretStatusQueue& turret_status_queue();
+  o2016::turret::TurretOutputQueue& turret_output_queue();
 
-    MessageQueue<muan::proto::StackProto<PdpStatus, 512>> pdp_status_queue_;
+  o2016::drivetrain::DrivetrainInputQueue& drivetrain_input_queue();
+  o2016::drivetrain::DrivetrainGoalQueue& drivetrain_goal_queue();
+  o2016::drivetrain::DrivetrainStatusQueue& drivetrain_status_queue();
+  o2016::drivetrain::DrivetrainOutputQueue& drivetrain_output_queue();
+
+  o2016::catapult::CatapultInputQueue& catapult_input_queue();
+  o2016::catapult::CatapultGoalQueue& catapult_goal_queue();
+  o2016::catapult::CatapultStatusQueue& catapult_status_queue();
+  o2016::catapult::CatapultOutputQueue& catapult_output_queue();
+
+  o2016::intake::IntakeInputQueue& intake_input_queue();
+  o2016::intake::IntakeGoalQueue& intake_goal_queue();
+  o2016::intake::IntakeStatusQueue& intake_status_queue();
+  o2016::intake::IntakeOutputQueue& intake_output_queue();
+
+ private:
+  QueueManager() = default;
+  ~QueueManager() = default;
+
+  MessageQueue<muan::proto::StackProto<PdpStatus, 512>> pdp_status_queue_;
+
+  o2016::turret::TurretInputQueue turret_input_queue_;
+  o2016::turret::TurretGoalQueue turret_goal_queue_;
+  o2016::turret::TurretStatusQueue turret_status_queue_;
+  o2016::turret::TurretOutputQueue turret_output_queue_;
+
+  o2016::drivetrain::DrivetrainInputQueue drivetrain_input_queue_;
+  o2016::drivetrain::DrivetrainGoalQueue drivetrain_goal_queue_;
+  o2016::drivetrain::DrivetrainStatusQueue drivetrain_status_queue_;
+  o2016::drivetrain::DrivetrainOutputQueue drivetrain_output_queue_;
+
+  o2016::catapult::CatapultInputQueue catapult_input_queue_;
+  o2016::catapult::CatapultGoalQueue catapult_goal_queue_;
+  o2016::catapult::CatapultStatusQueue catapult_status_queue_;
+  o2016::catapult::CatapultOutputQueue catapult_output_queue_;
+
+  o2016::intake::IntakeInputQueue intake_input_queue_;
+  o2016::intake::IntakeGoalQueue intake_goal_queue_;
+  o2016::intake::IntakeStatusQueue intake_status_queue_;
+  o2016::intake::IntakeOutputQueue intake_output_queue_;
 };
 
-}
+}  // o2016
 
 #endif
