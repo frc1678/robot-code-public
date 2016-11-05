520d5a6d499c23e52bc7dacaf424680514d836fc
Lucas Solomon <amannababanana@gmail.com>
2016-11-05 14:09:08 -0700

    Split linear interpolation into a .h file and a .hpp file

--- muan/utils/linear_interpolation.h
+++ muan/utils/linear_interpolation.h
@@ -3,53 +3,20 @@
 
 #include <vector>
 #include <utility>
-#include <stdexcept>
 
 namespace muan {
 
 template <typename T>
 class LinearInterpolation {
  public:
-  LinearInterpolation(std::vector<std::pair<double, T>> data) {
-    if(data.size() < 2) {
-      throw std::invalid_argument("Interpolate requires 2 or more control points");
-    }
-    data_ = data;
-  }
-  T operator()(double x) {
-    // index of greatest x-value less than x
-    int lower_bounds_index = -1;
-    // index of lowest x-value greater than x
-    int upper_bounds_index = -1;
-    for(int i = 0; i < data_.size(); i++) {
-      if(data_[i].first <= x && (lower_bounds_index == -1 || data_[lower_bounds_index].first < data_[i].first)) {
-        lower_bounds_index = i;
-      }
-      if(data_[i].first >= x && (upper_bounds_index == -1 || data_[upper_bounds_index].first > data_[i].first)) {
-        upper_bounds_index = i;
-      }
-    }
-    if(lower_bounds_index == -1 || upper_bounds_index == -1) {
-      throw std::domain_error("An interpolation is only defined between the lowest and highest x-values");
-    }
-    double x0 = data_[lower_bounds_index].first;
-    double x1 = data_[upper_bounds_index].first;
-    T y0 = data_[lower_bounds_index].second;
-    T y1 = data_[upper_bounds_index].second;
-
-    // Obtained as the solution to (y-y0)/(x-x0) = (y1-y0)/(x1-x0)
-    T y;
-    if(x0 == x1) {
-      y = y0;
-    } else {
-      y = y0 + (x - x0) * (y1 - y0) / (x1 - x0);
-    }
-    return y;
-  }
+  LinearInterpolation(std::vector<std::pair<double, T>> data);
+  T operator()(double x);
  protected:
   std::vector<std::pair<double, T>> data_;
 };
 
 } // muan
+
+#include "linear_interpolation.hpp"
 
 #endif // MUAN_UTILS_LINEAR_INTERPOLATION_
