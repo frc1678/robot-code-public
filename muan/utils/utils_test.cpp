bae754a4088a11a8ece550c733a159c451417772
Lucas Solomon <amannababanana@gmail.com>
2016-12-02 18:05:05 -0800

    Update test for linear interpolation

--- muan/utils/utils_test.cpp
+++ muan/utils/utils_test.cpp
@@ -67,9 +67,10 @@ TEST(LinearInterpolation, VerifiesListSize) {
 
 TEST(LinearInterpolation, CalculatesCorrectly) {
   LinearInterpolation<double> f({std::make_pair(3., 2.),
-                                 std::make_pair(0., 0.),
-                                 std::make_pair(1., 0.),
-                                 std::make_pair(6., 8.)});
+                                 std::make_pair(1., 0.)});
+  f.AddControlPoint(std::make_pair(0., 0.));
+  f.AddControlPoint(std::make_pair(7., 7.));
+  f.AddControlPoint(std::make_pair(6., 8.));
   EXPECT_EQ(f.lower_boundary(), 0.);
   EXPECT_EQ(f.upper_boundary(), 6.);
   EXPECT_EQ(f(0), 0);
@@ -79,5 +80,6 @@ TEST(LinearInterpolation, CalculatesCorrectly) {
   EXPECT_EQ(f(4), 4);
   EXPECT_EQ(f(5), 6);
   EXPECT_EQ(f(6), 8);
+  EXPECT_EQ(f(7), 7);
   EXPECT_DEATH(f(100), "An interpolation is only defined between the lowest and highest x-values");
 }
