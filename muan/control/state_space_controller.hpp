4ba7e6bd61cd3a162b40f74fa109c3e60fa0f94e
Wesley Aptekar-Cassels <W.Aptekar@gmail.com>
2016-12-04 11:30:06 -0800

    Format all hpp files

--- muan/control/state_space_controller.hpp
+++ muan/control/state_space_controller.hpp
@@ -10,21 +10,17 @@ namespace muan {
 namespace control {
 
 template <uint32_t kNumInputs, uint32_t kNumStates, uint32_t kNumOutputs>
-StateSpaceController<kNumInputs, kNumStates,
-                     kNumOutputs>::StateSpaceController()
+StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::StateSpaceController()
     : K_{Eigen::Matrix<double, kNumInputs, kNumStates>::Zero()},
       Kff_{Eigen::Matrix<double, kNumInputs, kNumStates>::Zero()},
       A_{Eigen::Matrix<double, kNumStates, kNumStates>::Identity()},
       r_{Eigen::Matrix<double, kNumStates, 1>::Zero()},
-      u_min_{Eigen::Matrix<double, kNumInputs, 1>::Constant(
-          -std::numeric_limits<double>::infinity())},
-      u_max_{Eigen::Matrix<double, kNumInputs, 1>::Constant(
-          std::numeric_limits<double>::infinity())} {}
+      u_min_{Eigen::Matrix<double, kNumInputs, 1>::Constant(-std::numeric_limits<double>::infinity())},
+      u_max_{Eigen::Matrix<double, kNumInputs, 1>::Constant(std::numeric_limits<double>::infinity())} {}
 
 template <uint32_t kNumInputs, uint32_t kNumStates, uint32_t kNumOutputs>
 StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::StateSpaceController(
-    const Eigen::Matrix<double, kNumInputs, kNumStates>& K,
-    const Eigen::Matrix<double, kNumInputs, 1>& u_min,
+    const Eigen::Matrix<double, kNumInputs, kNumStates>& K, const Eigen::Matrix<double, kNumInputs, 1>& u_min,
     const Eigen::Matrix<double, kNumInputs, 1>& u_max)
     : K_{K},
       Kff_{Eigen::Matrix<double, kNumInputs, kNumStates>::Zero()},
@@ -37,8 +33,7 @@ template <uint32_t kNumInputs, uint32_t kNumStates, uint32_t kNumOutputs>
 StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::StateSpaceController(
     const Eigen::Matrix<double, kNumInputs, kNumStates>& K,
     const Eigen::Matrix<double, kNumInputs, kNumStates>& Kff,
-    const Eigen::Matrix<double, kNumStates, kNumStates>& A,
-    const Eigen::Matrix<double, kNumInputs, 1>& u_min,
+    const Eigen::Matrix<double, kNumStates, kNumStates>& A, const Eigen::Matrix<double, kNumInputs, 1>& u_min,
     const Eigen::Matrix<double, kNumInputs, 1>& u_max)
     : K_{K},
       Kff_{Kff},
@@ -48,94 +43,80 @@ StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::StateSpaceController(
       u_max_{u_max} {}
 
 template <uint32_t kNumInputs, uint32_t kNumStates, uint32_t kNumOutputs>
-Eigen::Matrix<double, kNumInputs, 1>
-StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::Update(
+Eigen::Matrix<double, kNumInputs, 1> StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::Update(
     const Eigen::Matrix<double, kNumStates, 1>& x) {
-  Eigen::Matrix<double, kNumInputs, 1> u =
-      K() * (r_ - x) + Kff() * (r_ - A() * r_);
+  Eigen::Matrix<double, kNumInputs, 1> u = K() * (r_ - x) + Kff() * (r_ - A() * r_);
   return CapMatrix(u, u_min(), u_max());
 }
 
 template <uint32_t kNumInputs, uint32_t kNumStates, uint32_t kNumOutputs>
-Eigen::Matrix<double, kNumInputs, 1>
-StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::Update(
-    const Eigen::Matrix<double, kNumStates, 1>& x,
-    const Eigen::Matrix<double, kNumStates, 1>& r) {
-  Eigen::Matrix<double, kNumInputs, 1> u =
-      K() * (r_ - x) + Kff() * (r - A() * r_);
+Eigen::Matrix<double, kNumInputs, 1> StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::Update(
+    const Eigen::Matrix<double, kNumStates, 1>& x, const Eigen::Matrix<double, kNumStates, 1>& r) {
+  Eigen::Matrix<double, kNumInputs, 1> u = K() * (r_ - x) + Kff() * (r - A() * r_);
   r_ = r;
   return CapMatrix(u, u_min(), u_max());
 }
 
 template <uint32_t kNumInputs, uint32_t kNumStates, uint32_t kNumOutputs>
-const Eigen::Matrix<double, kNumStates, 1>&
-StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::r() const {
+const Eigen::Matrix<double, kNumStates, 1>& StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::r()
+    const {
   return r_;
 }
 
 template <uint32_t kNumInputs, uint32_t kNumStates, uint32_t kNumOutputs>
-Eigen::Matrix<double, kNumStates, 1>&
-StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::r() {
+Eigen::Matrix<double, kNumStates, 1>& StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::r() {
   return r_;
 }
 
 template <uint32_t kNumInputs, uint32_t kNumStates, uint32_t kNumOutputs>
-double StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::r(
-    uint32_t i) const {
+double StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::r(uint32_t i) const {
   return r_(i);
 }
 
 template <uint32_t kNumInputs, uint32_t kNumStates, uint32_t kNumOutputs>
-double& StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::r(
-    uint32_t i) {
+double& StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::r(uint32_t i) {
   return r_(i);
 }
 
 template <uint32_t kNumInputs, uint32_t kNumStates, uint32_t kNumOutputs>
-const Eigen::Matrix<double, kNumInputs, 1>&
-StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::u_min() const {
+const Eigen::Matrix<double, kNumInputs, 1>& StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::u_min()
+    const {
   return u_min_;
 }
 
 template <uint32_t kNumInputs, uint32_t kNumStates, uint32_t kNumOutputs>
-Eigen::Matrix<double, kNumInputs, 1>&
-StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::u_min() {
+Eigen::Matrix<double, kNumInputs, 1>& StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::u_min() {
   return u_min_;
 }
 
 template <uint32_t kNumInputs, uint32_t kNumStates, uint32_t kNumOutputs>
-double StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::u_min(
-    uint32_t i) const {
+double StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::u_min(uint32_t i) const {
   return u_min_(i);
 }
 
 template <uint32_t kNumInputs, uint32_t kNumStates, uint32_t kNumOutputs>
-double& StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::u_min(
-    uint32_t i) {
+double& StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::u_min(uint32_t i) {
   return u_min_(i);
 }
 
 template <uint32_t kNumInputs, uint32_t kNumStates, uint32_t kNumOutputs>
-const Eigen::Matrix<double, kNumInputs, 1>&
-StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::u_max() const {
+const Eigen::Matrix<double, kNumInputs, 1>& StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::u_max()
+    const {
   return u_max_;
 }
 
 template <uint32_t kNumInputs, uint32_t kNumStates, uint32_t kNumOutputs>
-Eigen::Matrix<double, kNumInputs, 1>&
-StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::u_max() {
+Eigen::Matrix<double, kNumInputs, 1>& StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::u_max() {
   return u_max_;
 }
 
 template <uint32_t kNumInputs, uint32_t kNumStates, uint32_t kNumOutputs>
-double StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::u_max(
-    uint32_t i) const {
+double StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::u_max(uint32_t i) const {
   return u_max_(i);
 }
 
 template <uint32_t kNumInputs, uint32_t kNumStates, uint32_t kNumOutputs>
-double& StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::u_max(
-    uint32_t i) {
+double& StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::u_max(uint32_t i) {
   return u_max_(i);
 }
 
@@ -152,14 +133,12 @@ StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::K() {
 }
 
 template <uint32_t kNumInputs, uint32_t kNumStates, uint32_t kNumOutputs>
-double StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::K(
-    uint32_t i, uint32_t j) const {
+double StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::K(uint32_t i, uint32_t j) const {
   return K_(i, j);
 }
 
 template <uint32_t kNumInputs, uint32_t kNumStates, uint32_t kNumOutputs>
-double& StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::K(
-    uint32_t i, uint32_t j) {
+double& StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::K(uint32_t i, uint32_t j) {
   return K_(i, j);
 }
 
@@ -176,14 +155,12 @@ StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::Kff() {
 }
 
 template <uint32_t kNumInputs, uint32_t kNumStates, uint32_t kNumOutputs>
-double StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::Kff(
-    uint32_t i, uint32_t j) const {
+double StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::Kff(uint32_t i, uint32_t j) const {
   return Kff_(i, j);
 }
 
 template <uint32_t kNumInputs, uint32_t kNumStates, uint32_t kNumOutputs>
-double& StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::Kff(
-    uint32_t i, uint32_t j) {
+double& StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::Kff(uint32_t i, uint32_t j) {
   return Kff_(i, j);
 }
 
@@ -200,14 +177,12 @@ StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::A() {
 }
 
 template <uint32_t kNumInputs, uint32_t kNumStates, uint32_t kNumOutputs>
-double StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::A(
-    uint32_t i, uint32_t j) const {
+double StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::A(uint32_t i, uint32_t j) const {
   return A_(i, j);
 }
 
 template <uint32_t kNumInputs, uint32_t kNumStates, uint32_t kNumOutputs>
-double& StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::A(
-    uint32_t i, uint32_t j) {
+double& StateSpaceController<kNumInputs, kNumStates, kNumOutputs>::A(uint32_t i, uint32_t j) {
   return A_(i, j);
 }
 
