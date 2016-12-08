192aabdec2c6a7a399264792aaec29f44400dbfd
Wesley Aptekar-Cassels <W.Aptekar@gmail.com>
2016-12-07 17:44:46 -0800

    Comment formatting check in tests.sh
    Since we don't have a good method for controlling the version of
    clang-format on people's computers (and on travis), I'm commenting the
    check for formatted code in the testing script.

--- tests.sh
+++ tests.sh
@@ -3,4 +3,4 @@ bazel test --test_output=errors //muan/... -- -//muan/wpilib/... -//muan/teleop/
 bazel build //muan/... --cpu=roborio
 bazel test --test_output=errors //o2016/... -- -//o2016:frc1678 -//o2016/wpilib/... -//o2016/subsystem_runner/... -//o2016/teleop/...
 bazel build //o2016/... --cpu=roborio
-./check-format.py
+#./check-format.py
