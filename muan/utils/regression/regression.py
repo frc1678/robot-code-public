Remove incorrect noise calculation

muan/utils/regression/regression.py
@@ -38,7 +38,4 @@ def state_space_regression(data, numstates):
     gains_A = x[:numstates, :].T
     gains_B = x[numstates:, :].T
 
-    error = A.dot(x) - b
-    Q_noise = np.diag(error.std(0))
-
-    return gains_A, gains_B, Q_noise
+    return gains_A, gains_B

muan/utils/regression/regression_main.py
@@ -21,5 +21,5 @@ u = csv[:, [m.start() for m in re.finditer("u", order)]]
 data = np.hstack((x, u))
 numstates = len([m.start() for m in re.finditer("x", order)])
 
-A, B, Q_noise = regression.state_space_regression(data, numstates)
-np.savez("system_matrices", A=A, B=B, Q_noise=Q_noise)
+A, B = regression.state_space_regression(data, numstates)
+np.savez("system_matrices", A=A, B=B)