436c696bf448d08ad2fa96e2ed89169e7a4100e7
Lucas Solomon <amannababanana@gmail.com>
2016-12-16 16:15:31 -0800

    Add test for a dense A matrix

--- muan/utils/regression/regression_test.py
+++ muan/utils/regression/regression_test.py
@@ -28,14 +28,14 @@ def test_simple_system():
     np.testing.assert_array_almost_equal(B, goal_B)
 
 def test_organizing():
-    labels =          'x'  'u' '_' 'x' 'u'
-    data = np.matrix([[1,   1,  4,  3,  0],
-                      [2,   3,  5,  3, -1],
-                      [5,   1,  8,  1,  1],
-                      [6,   1,  2,  3, -2],
-                      [7,   5, 13, -1, -1],
+    labels =           'x' 'u' '_' 'x' 'u'
+    data = np.matrix([[ 1,  1,  4,  3,  0],
+                      [ 2,  3,  5,  3, -1],
+                      [ 5,  1,  8,  1,  1],
+                      [ 6,  1,  2,  3, -2],
+                      [ 7,  5, 13, -1, -1],
                       [12, -6, 14, -3, -1],
-                      [6,   0, 14, -5,  0]])
+                      [ 6,  0, 14, -5,  0]])
 
     xt, ut = regression.seperate_data(data, labels)
     A, B = regression.state_space_regression(xt, ut)
@@ -49,5 +49,24 @@ def test_organizing():
     np.testing.assert_array_almost_equal(A, goal_A)
     np.testing.assert_array_almost_equal(B, goal_B)
 
+def test_dense_matrix():
+    labels =           'x' 'x' 'x'
+    data = np.matrix([[ 2,  3,  0],
+                      [-1,  5,  3],
+		      [-3,  4,  8],
+		      [ 1,  1, 12],
+		      [12,  2, 13],
+		      [23, 14, 15]])
+
+    xt, ut = regression.seperate_data(data, labels)
+    A, B = regression.state_space_regression(xt, ut)
+
+    goal_A = np.matrix([[1, -1, 1],
+                        [1,  1, 0],
+                        [0,  1, 1]])
+
+    np.testing.assert_array_almost_equal(A, goal_A)
+
 test_simple_system()
 test_organizing()
+test_dense_matrix()
