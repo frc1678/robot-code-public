d53b8a1c6c573a53ccb76c055d3aef51fc77834d
DevBurrito <jishnu1@gmail.com>
2017-01-05 10:02:54 -0800

    Bring back old logging code (not sure why it was deleted)

---
+++ muan/logging/log.h
@@ -0,0 +1,57 @@
+/*
+ * log.h
+ *
+ *  Created on: Oct 1, 2015
+ *  Copyright 2015 Citrus Circuits
+ *      Author: Kyle Stachowicz
+ */
+
+#ifndef MUAN_LOGGING_LOG_H_
+#define MUAN_LOGGING_LOG_H_
+
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <mutex>
+#include <sstream>
+#include <string>
+#include <thread>
+
+// #define LOG_VERBOSE
+
+#ifdef LOG_VERBOSE
+#define CODE_STAMP \
+  " (at line " + std::to_string(__LINE__) + " of " + __FILE__ + " in function " + __PRETTY_FUNCTION__ + ")"
+#else
+#define CODE_STAMP ""
+#endif
+
+namespace muan {
+
+/*
+ * The base class for logs. Automatically flushes to a file every half second.
+ * To use, implement FlushToFile() and GetExtension()
+ */
+class Log {
+ public:
+  Log(std::string name, std::string extension);
+  virtual void FlushToFile() = 0;
+  virtual std::string GetExtension() const = 0;
+  const std::string& GetName() const;
+  virtual ~Log();
+
+ protected:
+  static std::string GetDateString();
+  static std::string GetTimeString();
+
+  static std::string folder_path_;
+  static std::once_flag folder_created_;
+
+  std::ofstream file_;
+  std::mutex mutex_;
+  std::string name_;
+};
+
+}  // namespace muan
+
+#endif  // MUAN_LOGGING_LOG_H_
