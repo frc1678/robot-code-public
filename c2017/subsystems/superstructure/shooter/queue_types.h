8c88dba65e90547153ab01e5460a073648f5787b
jishnusen <jishnu1@gmail.com>
2017-01-11 21:54:46 -0800

    Added BUILD and queue_types.h

---
+++ c2017/subsystems/superstructure/shooter/queue_types.h
@@ -0,0 +1,24 @@
+#ifndef C2017_SUBSYSTEMS_SUPERSTRUCTURE_SHOOTER_QUEUETYPES_H_
+#define C2017_SUBSYSTEMS_SUPERSTRUCTURE_SHOOTER_QUEUETYPES_H_
+
+#include "muan/proto/stack_proto.h"
+#include "muan/queues/message_queue.h"
+#include "c2017/subsystems/superstructure/shooter/shooter.pb.h"
+
+namespace c2017 {
+
+namespace shooter {
+
+using ShooterGoalProto = muan::proto::StackProto<::c2017::shooter::ShooterGoal, 256>;
+using ShooterInputProto = muan::proto::StackProto<::c2017::shooter::ShooterInput, 256>;
+using ShooterStatusProto = muan::proto::StackProto<::c2017::shooter::ShooterStatus, 256>;
+using ShooterOutputProto = muan::proto::StackProto<::c2017::shooter::ShooterOutput, 256>;
+
+using ShooterInputQueue = muan::queues::MessageQueue<ShooterInputProto, 200>;
+using ShooterGoalQueue = muan::queues::MessageQueue<ShooterGoalProto, 200>;
+using ShooterOutputQueue = muan::queues::MessageQueue<ShooterOutputProto, 200>;
+using ShooterStatusQueue = muan::queues::MessageQueue<ShooterStatusProto, 200>;
+}
+}
+
+#endif  // C2017_SUBSYSTEMS_SUPERSTRUCTURE_SHOOTER_QUEUETYPES_H_
