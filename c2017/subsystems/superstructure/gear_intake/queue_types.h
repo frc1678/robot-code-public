d1c3916b8f2756492865050720246bd23037a7d8
Daviji <Dolbear61@yahoo.com>
2017-01-12 04:54:27 +0000

    Add gear intake subsystem

---
+++ c2017/subsystems/superstructure/gear_intake/queue_types.h
@@ -0,0 +1,25 @@
+#ifndef C2017_SUBSYSTEMS_SUPERSTRUCTURE_GEAR_INTAKE_QUEUETYPES_H_
+#define C2017_SUBSYSTEMS_SUPERSTRUCTURE_GEAR_INTAKE_QUEUETYPES_H_
+
+#include "muan/proto/stack_proto.h"
+#include "muan/queues/message_queue.h"
+#include "c2017/subsystems/superstructure/gear_intake/intake.pb.h"
+
+namespace c2017 {
+
+namespace gear_intake {
+
+using GearIntakeOutputProto = muan::proto::StackProto<GearIntakeOutput, 256>;
+using GearIntakeInputProto = muan::proto::StackProto<GearIntakeInput, 256>;
+using GearIntakeStatusProto = muan::proto::StackProto<GearIntakeStatus, 256>;
+using GearIntakeGoalProto = muan::proto::StackProto<GearIntakeGoal, 256>;
+
+using GearIntakeOutputQueue = muan::queues::MessageQueue<GearIntakeOutputProto, 200>;
+using GearIntakeInputQueue = muan::queues::MessageQueue<GearIntakeInputProto, 200>;
+using GearIntakeStatusQueue = muan::queues::MessageQueue<GearIntakeStatusProto, 200>;
+using GearIntakeGoalQueue = muan::queues::MessageQueue<GearIntakeGoalProto, 200>;
+
+}
+
+}
+#endif
