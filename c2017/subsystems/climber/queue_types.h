d7a012cc20812c2e8b878d663f11808c51488a62
Gemma Bertain <gemma@bertain.net>
2017-01-13 03:58:24 +0000

    added queues

--- c2017/subsystems/climber/queue_types.h
+++ c2017/subsystems/climber/queue_types.h
@@ -14,6 +14,10 @@ using ClimberInputProto = muan::proto::StackProto<ClimberInput, 1024>;
 using ClimberStatusProto = muan::proto::StackProto<ClimberStatus, 1024>;
 using ClimberGoalProto = muan::proto::StackProto<ClimberGoal, 1024>;
 
+using ClimberOutputQueue = muan::queues::MessageQueue<ClimberOutputProto, 100>;
+using ClimberInputQueue = muan::queues::MessageQueue<ClimberInputProto, 100>;
+using ClimberStatusQueue = muan::queues::MessageQueue<ClimberStatusProto, 100>;
+using ClimberGoalQueue = muan::queues::MessageQueue<ClimberGoalProto, 100>;
 }  // climber
 
 }  // c2017
