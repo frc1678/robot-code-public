7657c65a1e74e82fe1e60654cc7fc39b04d086e8
Wesley Aptekar-Cassels <W.Aptekar@gmail.com>
2017-01-13 00:39:32 -0800

    Add drivetrain subsystem to c2017

--- c2017/subsystem_runner/subsystem_runner.cpp
+++ c2017/subsystem_runner/subsystem_runner.cpp
@@ -2,6 +2,15 @@
 
 namespace c2017 {
 
+SubsystemRunner::SubsystemRunner()
+    : drivetrain_{::c2017::drivetrain::GetDrivetrainConfig(),
+                  QueueManager::GetInstance()->drivetrain_goal_queue(),
+                  QueueManager::GetInstance()->drivetrain_input_queue(),
+                  QueueManager::GetInstance()->drivetrain_output_queue(),
+                  QueueManager::GetInstance()->drivetrain_status_queue(),
+                  QueueManager::GetInstance()->driver_station_queue(),
+                  QueueManager::GetInstance()->gyro_queue()} {}
+
 void SubsystemRunner::operator()() {
   aos::time::PhasedLoop phased_loop(aos::time::Time::InMS(5));
 
@@ -13,7 +22,9 @@ void SubsystemRunner::operator()() {
 
   while (running_) {
     wpilib_.ReadSensors();
+
     // Update subsystems here
+    drivetrain_.Update();
 
     wpilib_.WriteActuators();
 
--- c2017/subsystem_runner/subsystem_runner.h
+++ c2017/subsystem_runner/subsystem_runner.h
@@ -8,12 +8,13 @@
 #include "third_party/aos/common/time.h"
 #include "third_party/aos/common/util/phased_loop.h"
 #include "third_party/aos/linux_code/init.h"
+#include "third_party/frc971/control_loops/drivetrain/drivetrain.h"
 
 namespace c2017 {
 
 class SubsystemRunner {
  public:
-  SubsystemRunner() = default;
+  SubsystemRunner();
   ~SubsystemRunner() = default;
   void operator()();
   void Stop();
@@ -21,6 +22,7 @@ class SubsystemRunner {
  private:
   std::atomic<bool> running_;
   wpilib::WpilibInterface wpilib_;
+  frc971::control_loops::drivetrain::DrivetrainLoop drivetrain_;
 };
 }
 #endif
