6f03d08e0f8977e79428867e20c8b7225adc5286
Alex Hille <alex_hill20@icloud.com>
2017-01-13 05:03:31 +0000

    Added BUILD file and deleted elevator challenge associated files

--- test/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "main.h"
-
-Robot::TeleopPeriodic() {
-    outputs = drivetrain.Update(throttle.GetY();, wheel.GetX();, wheel.GetRawButton(0);)
-    drive.TankDrive(outputs.left_voltage, outputs.right_voltage)
-}
-
