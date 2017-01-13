d23f986be717b6c8a892fdfff0301411724caf07
Alex Hille <alex_hill20@icloud.com>
2017-01-13 04:32:02 +0000

    Added subsystems/magazine folder. Also added a magazine proto and it's queue types file.

---
+++ c2017/subsystems/magazine/magazine1.cpp
@@ -0,0 +1,9 @@
+//
+//  magazine1.cpp
+//  
+//
+//  Created by Alex Hill on 1/12/17.
+//
+//
+
+#include "magazine1.hpp"
---
+++ c2017/subsystems/magazine/magazine1.hpp
@@ -0,0 +1,14 @@
+//
+//  magazine1.hpp
+//  
+//
+//  Created by Alex Hill on 1/12/17.
+//
+//
+
+#ifndef magazine1_hpp
+#define magazine1_hpp
+
+#include <stdio.h>
+
+#endif /* magazine1_hpp */
