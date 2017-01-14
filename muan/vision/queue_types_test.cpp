28ad4ed27e245387be7ab60eb9e45deea489b3d6
Lucas Solomon <amannababanana@gmail.com>
2017-01-14 23:56:25 +0000

    Add vision protos and queues

---
+++ muan/vision/queue_types_test.cpp
@@ -0,0 +1,7 @@
+#include "queue_types.h"
+#include "gtest/gtest.h"
+
+TEST(Types, CanConstruct) {
+  muan::vision::VisionPositionProto position;
+  muan::vision::VisionPositionQueue position_queue;
+}
