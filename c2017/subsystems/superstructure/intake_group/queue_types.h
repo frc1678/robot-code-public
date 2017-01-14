9723c7cad29f9c411770ee59907419720811b687
Nathan Solomon <nathansolomon1678@gmail.com>
2017-01-14 13:52:28 -0800

    Create IntakeGroup proto

---
+++ c2017/subsystems/superstructure/intake_group/queue_types.h
@@ -0,0 +1,17 @@
+#ifndef C2017_SUBSYSTEMS_SUPERSTRUCTURE_INTAKE_GROUP_QUEUETYPES_H_
+#define C2017_SUBSYSTEMS_SUPERSTRUCTURE_INTAKE_GROUP_QUEUETYPES_H_
+
+#include "muan/proto/stack_proto.h"
+#include "muan/queues/message_queue.h"
+#include "c2017/subsystems/superstructure/intake_group/intake_group.pb.h"
+
+namespace c2017 {
+  
+  namespace intake_group {
+
+  using IntakeGroupGoalProto = muan::proto::StackProto<IntakeGoal, 256>;
+
+  using IntakeGroupGoalQueue = muan::queues::MessageQueue<IntakeGoalProto, 200>;
+  }
+}
+#endif//C2017_SUBSYSTEMS_SUPERSTRUCTURE_INTAKE_GROUP_QUEUETYPES_H_
