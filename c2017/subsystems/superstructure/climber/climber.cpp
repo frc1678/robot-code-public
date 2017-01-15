c6b4a6a4e684287b61bf3e24d08b5daf938b99af
Gemma Bertain <gemma@bertain.net>
2017-01-15 23:36:31 +0000

    Moved the climber directory into the superstructure directory

---
+++ c2017/subsystems/superstructure/climber/climber.cpp
@@ -0,0 +1,44 @@
+#include "climber.h"
+
+using namespace c2017;
+using namespace climber;
+
+namespace c2017 {
+
+namespace climber {
+Climber::Climber() {
+        voltage_ = 0.0; 
+        at_top_ = false;
+        is_climbing_ = false;
+    }
+void Climber::SetGoal (const ClimberGoalProto& goal) {
+  
+  goal_ = goal;
+}
+
+void Climber::Update (const ClimberInputProto& input) {
+    
+        bool to_climb = goal_->climbing();
+        if(to_climb){
+            is_climbing_ = true;
+            if (input->current() < 134) { // TODO tune current spike #
+                at_top_ = false;
+                voltage_ = 12.0; // TODO determine the direction for the climbing motors/shooter motors
+            } else { 
+                voltage_ = 0.0;
+                at_top_ = true;
+                is_climbing_ = true;
+            }
+        } else {
+            is_climbing_ = false;
+        }
+      output_->set_voltage(voltage_);
+      status_->set_currently_climbing(is_climbing_);
+      status_->set_hit_top(at_top_);
+    }  // Update
+    ClimberOutputProto Climber::Output() { return output_; } 
+    ClimberStatusProto Climber::Status() { return status_; }
+
+}  // climber
+
+}  // c2017
