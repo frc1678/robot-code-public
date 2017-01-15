da26992f225f52ec3164dd9e508444b454fd93ee
Wesley Aptekar-Cassels <W.Aptekar@gmail.com>
2017-01-15 13:46:28 -0800

    Update c2017 wpilib interface

--- c2017/wpilib/wpilib_interface.cpp
+++ c2017/wpilib/wpilib_interface.cpp
@@ -8,21 +8,73 @@ namespace wpilib {
 
 namespace ports {
 
+namespace drivetrain {
+
+constexpr uint32_t kMotorLeft = 1;
+constexpr uint32_t kMotorRight = 0;
+
+constexpr uint32_t kEncoderLeftA = 12, kEncoderLeftB = 13;
+constexpr uint32_t kEncoderRightA = 10, kEncoderRightB = 11;
+
+}  // drivetrain
+
 }  // ports
 
 constexpr double kMaxVoltage = 4;  // 4 volt bringup voltage
 
+
+DrivetrainInterface::DrivetrainInterface()
+    : input_queue_(QueueManager::GetInstance().drivetrain_input_queue()),
+      output_queue_(QueueManager::GetInstance().drivetrain_output_queue()->MakeReader()),
+      motor_left_{ports::drivetrain::kMotorLeft},
+      motor_right_{ports::drivetrain::kMotorRight},
+      encoder_left_{ports::drivetrain::kEncoderLeftA,
+                    ports::drivetrain::kEncoderLeftB},
+      encoder_right_{ports::drivetrain::kEncoderRightA,
+                     ports::drivetrain::kEncoderRightB} {
+}
+
+void DrivetrainInterface::ReadSensors() {
+  frc971::control_loops::drivetrain::InputProto sensors;
+  constexpr double wheel_radius = 3 * muan::units::in;
+  constexpr double kMetersPerClick = M_PI * 2.0 * wheel_radius / 360.0;
+  sensors->set_left_encoder(encoder_left_.Get() * kMetersPerClick);
+  sensors->set_right_encoder(-encoder_right_.Get() * kMetersPerClick);
+
+  input_queue_->WriteMessage(sensors);
+}
+
+void DrivetrainInterface::WriteActuators() {
+  auto outputs = output_queue_.ReadLastMessage();
+  if (outputs) {
+    motor_left_.Set(
+        -muan::Cap((*outputs)->left_voltage(), -kMaxVoltage, kMaxVoltage) /
+        12.0);
+
+    motor_right_.Set(
+        muan::Cap((*outputs)->right_voltage(), -kMaxVoltage, kMaxVoltage) /
+        12.0);
+  } else {
+    motor_left_.Set(0);
+    motor_left_.Set(0);
+  }
+}
+
+
 WpilibInterface::WpilibInterface()
-    : can_{&QueueManager::GetInstance().pdp_status_queue()} {
+    : can_{&QueueManager::GetInstance().pdp_status_queue()},
+      gyro_{QueueManager::GetInstance().gyro_queue()} {
+
   std::thread can_thread(std::ref(can_));
   can_thread.detach();
+
+  std::thread gyro_thread(std::ref(gyro_));
+  gyro_thread.detach();
 }
 
-void WpilibInterface::WriteActuators() {
-}
+void WpilibInterface::WriteActuators() { drivetrain_.WriteActuators(); }
 
-void WpilibInterface::ReadSensors() {
-}
+void WpilibInterface::ReadSensors() { drivetrain_.ReadSensors(); }
 
 }  // wpilib
 
--- c2017/wpilib/wpilib_interface.h
+++ c2017/wpilib/wpilib_interface.h
@@ -3,10 +3,31 @@
 
 #include "WPILib.h"
 #include "muan/wpilib/can_wrapper.h"
+#include "muan/wpilib/gyro/gyro_reader.h"
+#include "c2017/queue_manager/queue_manager.h"
 
 namespace c2017 {
 
 namespace wpilib {
+
+class DrivetrainInterface {
+ public:
+  DrivetrainInterface();
+
+  void WriteActuators();
+  void ReadSensors();
+
+ private:
+  muan::wpilib::PcmWrapper* pcm_;
+
+  frc971::control_loops::drivetrain::InputQueue* input_queue_;
+  frc971::control_loops::drivetrain::OutputQueue::QueueReader output_queue_;
+
+  VictorSP motor_left_;
+  VictorSP motor_right_;
+
+  Encoder encoder_left_, encoder_right_;
+};
 
 class WpilibInterface {
  public:
@@ -17,6 +38,9 @@ class WpilibInterface {
 
  private:
   muan::wpilib::CanWrapper can_;
+
+  muan::wpilib::gyro::GyroReader gyro_;
+  DrivetrainInterface drivetrain_;
 };
 
 }  // wpilib
