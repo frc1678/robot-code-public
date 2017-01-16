e05fe433e202c2eee374ff7470c897d9c19c77f7
jishnusen <jishnu1@gmail.com>
2017-01-16 14:58:20 -0800

    Format code

--- c2017/subsystems/superstructure/shooter/shooter_controller_test.cpp
+++ c2017/subsystems/superstructure/shooter/shooter_controller_test.cpp
@@ -15,16 +15,17 @@ TEST(ShooterControllerTest, IsSane) {
 
   c2017::shooter::ShooterController shooter_;
 
-  auto plant = muan::control::StateSpacePlant<1, 3, 1>(frc1678::shooter_controller::controller::A(), frc1678::shooter_controller::controller::B(), frc1678::shooter_controller::controller::C());
+  auto plant = muan::control::StateSpacePlant<1, 3, 1>(frc1678::shooter_controller::controller::A(),
+                                                       frc1678::shooter_controller::controller::B(),
+                                                       frc1678::shooter_controller::controller::C());
 
   plant.x(0) = 0.5;
   plant.x(1) = 0.0;
   plant.x(2) = 0.0;
 
-
-  for(int i = 0; i <= 1e5; i++) {
+  for (int i = 0; i <= 1e5; i++) {
     input->set_encoder_position(plant.x(0));
-  
+
     goal->set_goal_velocity(300.0);
     goal->set_goal_mode(c2017::shooter::ShooterGoal::FENDER);
 
@@ -32,13 +33,11 @@ TEST(ShooterControllerTest, IsSane) {
 
     output = shooter_.Update(input, ds);
     status = shooter_.get_status();
-    
-    plant.Update((Eigen::Matrix<double, 1, 1>() << output->voltage()).finished());
 
+    plant.Update((Eigen::Matrix<double, 1, 1>() << output->voltage()).finished());
 
     EXPECT_NEAR(output->voltage(), 0., 12.);
   }
-
 
   EXPECT_NEAR(status->observed_velocity(), 300, 10);
 }
@@ -54,16 +53,17 @@ TEST(ShooterControllerTest, CantTakeNegativeVoltage) {
 
   c2017::shooter::ShooterController shooter_;
 
-  auto plant = muan::control::StateSpacePlant<1, 3, 1>(frc1678::shooter_controller::controller::A(), frc1678::shooter_controller::controller::B(), frc1678::shooter_controller::controller::C());
+  auto plant = muan::control::StateSpacePlant<1, 3, 1>(frc1678::shooter_controller::controller::A(),
+                                                       frc1678::shooter_controller::controller::B(),
+                                                       frc1678::shooter_controller::controller::C());
 
   plant.x(0) = 0.5;
   plant.x(1) = 0.0;
   plant.x(2) = 0.0;
 
-
-  for(int i = 0; i <= 1e5; i++) {
+  for (int i = 0; i <= 1e5; i++) {
     input->set_encoder_position(plant.x(0));
-  
+
     goal->set_goal_velocity(-300.0);
     goal->set_goal_mode(c2017::shooter::ShooterGoal::FENDER);
 
@@ -71,9 +71,8 @@ TEST(ShooterControllerTest, CantTakeNegativeVoltage) {
 
     output = shooter_.Update(input, ds);
     status = shooter_.get_status();
-    
-    plant.Update((Eigen::Matrix<double, 1, 1>() << output->voltage()).finished());
 
+    plant.Update((Eigen::Matrix<double, 1, 1>() << output->voltage()).finished());
 
     EXPECT_NEAR(output->voltage(), 0., 12.);
   }
@@ -93,16 +92,17 @@ TEST(ShooterControllerTest, CanStop) {
 
   c2017::shooter::ShooterController shooter_;
 
-  auto plant = muan::control::StateSpacePlant<1, 3, 1>(frc1678::shooter_controller::controller::A(), frc1678::shooter_controller::controller::B(), frc1678::shooter_controller::controller::C());
+  auto plant = muan::control::StateSpacePlant<1, 3, 1>(frc1678::shooter_controller::controller::A(),
+                                                       frc1678::shooter_controller::controller::B(),
+                                                       frc1678::shooter_controller::controller::C());
 
   plant.x(0) = 0.5;
   plant.x(1) = 0.0;
   plant.x(2) = 0.0;
 
-
-  for(int i = 0; i <= 1e5; i++) {
+  for (int i = 0; i <= 1e5; i++) {
     input->set_encoder_position(plant.x(0));
-  
+
     goal->set_goal_velocity(0.0);
     goal->set_goal_mode(c2017::shooter::ShooterGoal::FENDER);
 
@@ -110,14 +110,11 @@ TEST(ShooterControllerTest, CanStop) {
 
     output = shooter_.Update(input, ds);
     status = shooter_.get_status();
-    
-    plant.Update((Eigen::Matrix<double, 1, 1>() << output->voltage()).finished());
 
+    plant.Update((Eigen::Matrix<double, 1, 1>() << output->voltage()).finished());
 
     EXPECT_NEAR(output->voltage(), 0., 12.);
   }
 
-
   EXPECT_NEAR(status->observed_velocity(), 0, 1e-5);
 }
-
