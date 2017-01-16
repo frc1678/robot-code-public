5f43b6de8f0a5241c850699401670cd9aded59ca
livyt <livytaylor47@gmail.com>
2017-01-16 21:58:19 +0000

    Run clang-format and remove at_goal_
    at_goal is never used, so I removed it. Clang changed some parameter lists.

--- c2017/subsystems/superstructure/trigger/trigger_controller.cpp
+++ c2017/subsystems/superstructure/trigger/trigger_controller.cpp
@@ -5,40 +5,47 @@ namespace c2017 {
 
 namespace trigger {
 
+//Constructor
 TriggerController::TriggerController() {
-  auto ss_plant = muan::control::StateSpacePlant<1, 3, 1>(frc1678::trigger_controller::controller::A(), frc1678::trigger_controller::controller::B(), frc1678::trigger_controller::controller::C());
-  
-  // matrix math I don't understand and hope is correct
+  auto ss_plant = muan::control::StateSpacePlant<1, 3, 1>(frc1678::trigger_controller::controller::A(),
+                                                          frc1678::trigger_controller::controller::B(),
+                                                          frc1678::trigger_controller::controller::C());
+
+  // Matrix math
   controller_ = muan::control::StateSpaceController<1, 3, 1>(frc1678::trigger_controller::controller::K());
   controller_.u_min() = Eigen::Matrix<double, 1, 1>::Ones() * -12.0;
   controller_.u_max() = Eigen::Matrix<double, 1, 1>::Ones() * 12.0;
-  observer_ = muan::control::StateSpaceObserver<1, 3, 1>(ss_plant, frc1678::trigger_controller::controller::L());
-  
+  observer_ =
+      muan::control::StateSpaceObserver<1, 3, 1>(ss_plant, frc1678::trigger_controller::controller::L());
+
   // Tolerance in rad/sec
   velocity_tolerance_ = 5;
-  at_goal_ = false;
 }
 
 TriggerOutputProto TriggerController::Update(const TriggerInputProto& input,
                                              const muan::wpilib::DriverStationProto& robot_state) {
   TriggerOutputProto output;
   TriggerStatusProto status;
-  
-  status->set_observed_velocity(observer_.x()[1]);
-  status->set_goal_velocity(16 * muan::units::pi / 2);
 
+  status->set_observed_velocity(observer_.x()[1]);
+  status->set_goal_velocity(goal_->balls_per_second() * muan::units::pi / 2);
+
+  // Checking if E-Stop/brownout/disabled from driver station proto
+  // Trigger should not be running if any of these are true
   bool enable_outputs = !(robot_state->mode() == RobotMode::ESTOP ||
                           robot_state->mode() == RobotMode::DISABLED || robot_state->brownout());
 
-  output->set_voltage(0);
+  output->set_voltage(0); // The voltage will end up being zero if enable_outputs is false
 
   if (enable_outputs) {
-    // Matrix stuff! Woo!
+    // r is the goal
     Eigen::Matrix<double, 3, 1> r;
     r << 0.0, (muan::units::pi / 2 * goal_->balls_per_second()), 0.0;
+    //y is the input/sensor values
     Eigen::Matrix<double, 1, 1> y;
     y << input->encoder_position();
 
+    //u is the motor value output
     auto u = controller_.Update(observer_.x(), r);
 
     observer_.Update(u, y);
--- c2017/subsystems/superstructure/trigger/trigger_controller.h
+++ c2017/subsystems/superstructure/trigger/trigger_controller.h
@@ -16,27 +16,27 @@ namespace trigger {
 class TriggerController {
  public:
   TriggerController();
-  //Updates trigger status and output protos
-  TriggerOutputProto Update(const TriggerInputProto& input, const muan::wpilib::DriverStationProto& robot_state);
+  // Updates trigger status and output protos
+  TriggerOutputProto Update(const TriggerInputProto& input,
+                            const muan::wpilib::DriverStationProto& robot_state);
   void SetGoal(TriggerGoalProto goal) { goal_ = goal; }
-  
+
   TriggerStatusProto get_status() { return status_; }
   muan::units::AngularVelocity get_velocity_tolerance() { return velocity_tolerance_; }
+
  private:
-  //creates local protos for status and goal
+  // creates local protos for status and goal
   TriggerStatusProto status_;
   TriggerGoalProto goal_;
-  
+
   muan::control::StateSpaceController<1, 3, 1> controller_;
-  muan::control::StateSpaceObserver<1,3,1> observer_;
-  
-  bool at_goal_;
+  muan::control::StateSpaceObserver<1, 3, 1> observer_;
 
   muan::units::AngularVelocity velocity_tolerance_;
 };
 
-} //trigger
+}  // trigger
 
-} //c2017
+}  // c2017
 
-#endif //C2017_SUBSYSTEMS_SUPERSTRUCTURE_TRIGGER_TRIGGER_CONTROLLER_H_
+#endif  // C2017_SUBSYSTEMS_SUPERSTRUCTURE_TRIGGER_TRIGGER_CONTROLLER_H_
