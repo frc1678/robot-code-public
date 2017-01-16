abcb2e22f4ae5332e838192738d06863919496c2
Daviji <Dolbear61@yahoo.com>
2017-01-16 18:10:04 +0000

    Fix naming and other minor problems

--- c2017/subsystems/superstructure/ball_intake/queue_types.h
+++ c2017/subsystems/superstructure/ball_intake/queue_types.h
@@ -13,11 +13,11 @@ using BallIntakeOutputProto = muan::proto::StackProto<BallIntakeOutput, 256>;
 using BallIntakeIntputProto = muan::proto::StackProto<BallIntakeInput, 256>;
 using BallIntakeStatusProto = muan::proto::StackProto<BallIntakeStatus, 256>;
 using BallIntakeGoalProto = muan::proto::StackProto<BallIntakeGoal, 256>;
-  
+
 using BallIntakeOutputQueue = muan::queues::MessageQueue<BallIntakeOutputProto, 100>;
 using BallIntakeInputQueue = muan::proto::StackProto<BallIntakeInputProto, 100>;
 using BallIntakeStatusQueue = muan::queues::MessageQueue<BallIntakeStatusProto, 100>;
 using BallIntakeGoalQueue = muan::queues::MessageQueue<BallIntakeGoalProto, 100>;
 }
-} 
-#endif//C2017_SUBSYSTEMS_SUPERSTRUCTURE_BALLINTAKE_QUEUETYPES_H_
+}
+#endif //C2017_SUBSYSTEMS_SUPERSTRUCTURE_BALLINTAKE_QUEUETYPES_H_
