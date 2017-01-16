32c317c83d2c21c122ef68483d0ba5b0b242cd97
Wesley Aptekar-Cassels <W.Aptekar@gmail.com>
2017-01-15 20:33:53 -0800

    Split drivetrain interface into seperate file

--- c2017/wpilib/wpilib_interface.h
+++ c2017/wpilib/wpilib_interface.h
@@ -7,28 +7,11 @@
 #include "c2017/queue_manager/queue_manager.h"
 #include "muan/utils/math_utils.h"
 
+#include "c2017/wpilib/drivetrain_interface.h"
+
 namespace c2017 {
 
 namespace wpilib {
-
-class DrivetrainInterface {
- public:
-  DrivetrainInterface();
-
-  void WriteActuators();
-  void ReadSensors();
-
- private:
-  muan::wpilib::PcmWrapper* pcm_;
-
-  frc971::control_loops::drivetrain::InputQueue* input_queue_;
-  frc971::control_loops::drivetrain::OutputQueue::QueueReader output_queue_;
-
-  VictorSP motor_left_;
-  VictorSP motor_right_;
-
-  Encoder encoder_left_, encoder_right_;
-};
 
 class WpilibInterface {
  public:
