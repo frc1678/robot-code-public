a3172b00aed9f03d7606be3a7757b3afec52c0e8
livyt <livytaylor47@gmail.com>
2017-01-21 01:34:20 +0000

    Remove dependency and run/fix clang
    PR comment stuff.

--- c2017/subsystems/superstructure/trigger/trigger_controller.cpp
+++ c2017/subsystems/superstructure/trigger/trigger_controller.cpp
@@ -6,10 +6,8 @@ namespace c2017 {
 
 namespace trigger {
 
-//Constructor
-TriggerController::TriggerController() :
-      status_queue_(QueueManager::GetInstance().trigger_status_queue()) 
-    {
+// Constructor
+TriggerController::TriggerController() : status_queue_(QueueManager::GetInstance().trigger_status_queue()) {
   auto ss_plant = muan::control::StateSpacePlant<1, 3, 1>(frc1678::trigger_controller::controller::A(),
                                                           frc1678::trigger_controller::controller::B(),
                                                           frc1678::trigger_controller::controller::C());
@@ -22,7 +20,7 @@ TriggerController::TriggerController() :
       muan::control::StateSpaceObserver<1, 3, 1>(ss_plant, frc1678::trigger_controller::controller::L());
 
   // Tolerance in rad/sec
-  velocity_tolerance_ = 1; //TODO tune this
+  velocity_tolerance_ = 1;  // TODO tune this
 }
 
 TriggerOutputProto TriggerController::Update(const TriggerInputProto& input,
@@ -33,13 +31,13 @@ TriggerOutputProto TriggerController::Update(const TriggerInputProto& input,
   // Checking if E-Stop/brownout/disabled from driver station proto
   // Trigger should not be running if any of these are true
   bool enable_outputs = !(robot_state->mode() == RobotMode::ESTOP ||
-                          robot_state->mode() == RobotMode::DISABLED ||
+                          robot_state->mode() == RobotMode::DISABLED || 
                           robot_state->brownout());
 
-    Eigen::Matrix<double, 1, 1> y;
-    y << input->encoder_position();
+  Eigen::Matrix<double, 1, 1> y;
+  y << input->encoder_position();
 
-  if (enable_outputs && balls_per_second_ > 0) { // I didn't see the point in adding another if statement that does the same thing as this one
+  if (enable_outputs && balls_per_second_ > 0) {  // I didn't see the point in adding another if statement that does the same thing as this one
     // r is the goal
     Eigen::Matrix<double, 3, 1> r;
     r << 0.0, ((muan::units::pi / 2) * balls_per_second_), 0.0;
@@ -51,7 +49,7 @@ TriggerOutputProto TriggerController::Update(const TriggerInputProto& input,
 
     // u is the motor value output
     auto u = controller_.Update(observer_.x(), r);
-    
+
     observer_.Update(u, y);
     output->set_voltage(u[0]);
   } else {
@@ -64,22 +62,16 @@ TriggerOutputProto TriggerController::Update(const TriggerInputProto& input,
   status->set_observed_velocity(observer_.x()[1]);
   status->set_goal_velocity(balls_per_second_ * muan::units::pi / 2);
   status->set_position(observer_.x()[0]);
-  
+
   status_queue_->WriteMessage(status);
   return output;
 }
 
-void TriggerController::SetGoal(TriggerGoalProto goal) { 
-  balls_per_second_ = goal->balls_per_second(); 
-}
+void TriggerController::SetGoal(TriggerGoalProto goal) { balls_per_second_ = goal->balls_per_second(); }
 
-muan::units::AngularVelocity TriggerController::get_velocity_tolerance() {
-  return velocity_tolerance_;
-}
+muan::units::AngularVelocity TriggerController::get_velocity_tolerance() { return velocity_tolerance_; }
 
-double TriggerController::get_bps() {
-  return balls_per_second_;
-}
+double TriggerController::get_bps() { return balls_per_second_; }
 
 }  // trigger
 
