233e8fc176ad96e998f4b5a11f5b244a7ecac7cb
Gemma Bertain <gemma@bertain.net>
2017-01-21 20:14:58 +0000

    Added the test fixture

--- c2017/subsystems/superstructure/climber/climber_test.cpp
+++ c2017/subsystems/superstructure/climber/climber_test.cpp
@@ -3,28 +3,49 @@
 #include <iostream>
 #include "c2017/queue_manager/queue_manager.h"
 
-TEST(Climbing, ClimbsToTheTop) {
+class ClimberTest : public ::testing::Test {
+ public:
+  ClimberTest() {}
+  void Update(double voltage) {
+     current_position_ += current_position_ > 1 ? 1 : 0.00042 * voltage;
+     current_ = voltage / 10;
+  }
+  double GetPosition() {
+    return current_position_;
+  }
+
+  void Reset() {
+    current_position_ = 0;
+    current_ = 0;
+  }
+
+ private:
+  double current_position_ = 0;
+  double current_ = 0;
+};
+
+TEST_F(ClimberTest, ClimbsToTheTop) {
   c2017::climber::ClimberGoalProto goal;
   c2017::climber::ClimberInputProto input;
-  c2017::climber::ClimberOutputProto output;
+
   muan::wpilib::DriverStationProto ds_status;
   ds_status->set_mode(RobotMode::TELEOP);
+
   goal->set_climbing(true);
-  input->set_position(100);  // position helps find the rate of the encoder. Encoder has been running throughout the match as the shooter so its big.
+  input->set_position(GetPosition());  // position helps find the rate of the encoder. Encoder has been running throughout the match as the shooter so its big.
+
   c2017::climber::Climber test_climber;
+
   test_climber.SetGoal(goal);
-  for (double t = 0.005; t < 2; t += 0.005) {
-    double d;
-    if (t < 1) {
-      d = t;
-    } else {
-      d = 1;
-    }
-    input->set_position(d);
-    output = test_climber.Update(input, ds_status);
-    if (d < 1) {
+
+  auto output = test_climber.Update(input, ds_status);
+  
+  for (double t = -1.005; t < 2; t += 0.005) {
+    input->set_position(GetPosition());
+    if (GetPosition() < 1) {
       EXPECT_NEAR(output->voltage(), 12, 1e-5);
     }
+    Update(output->voltage());
   }
 
   auto test_status = c2017::QueueManager::GetInstance().climber_status_queue().ReadLastMessage();
@@ -36,26 +57,26 @@ TEST(Climbing, ClimbsToTheTop) {
   EXPECT_NEAR(output->voltage(), 0, 1e-5);
 }
 
-TEST(Climbing, Disabled) {
+TEST_F(ClimberTest, Disabled) {
   c2017::climber::ClimberGoalProto goal;
   c2017::climber::ClimberInputProto input;
-  c2017::climber::ClimberOutputProto output;
+  
   muan::wpilib::DriverStationProto ds_status;
   ds_status->set_mode(RobotMode::DISABLED);
+  
   goal->set_climbing(true);
-  input->set_position(0);  // position helps find the rate of the encoder.
+  input->set_position(GetPosition());  // position helps find the rate of the encoder.
+  
   c2017::climber::Climber test_climber;
 
   test_climber.SetGoal(goal);
+  
+  auto output = test_climber.Update(input, ds_status);
   for (double t = 0; t < 2; t += 0.005) {
-    output = test_climber.Update(input, ds_status);
-    double d;
-    if (t < 1) {
-      d = t;
-    } else {
-      d = 1;
+    input->set_position(GetPosition());
+    if (GetPosition() < 1) {
+      EXPECT_NEAR(output->voltage(), 0, 1e-5);
     }
-    input->set_position(d);
     auto test_status = c2017::QueueManager::GetInstance().climber_status_queue().ReadLastMessage();
 
     if(test_status) {
