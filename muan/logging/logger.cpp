f341bf0fa633948371c628060a3f2d57c556b8cd
Wesley Aptekar-Cassels <W.Aptekar@gmail.com>
2017-01-21 11:22:18 -0800

    Split logger into a .cpp and a .hpp

---
+++ muan/logging/logger.cpp
@@ -0,0 +1,53 @@
+#include "muan/logging/logger.h"
+
+namespace muan {
+namespace logging {
+
+Logger::Logger() { writer_ = std::make_unique<FileWriter>(); }
+
+Logger::Logger(std::unique_ptr<FileWriter>&& writer) : writer_(std::move(writer)) {}
+
+void Logger::Run() {
+  aos::time::PhasedLoop phased_loop(aos::time::Time::InMS(20));
+
+  aos::SetCurrentThreadRealtimePriority(20);
+  aos::SetCurrentThreadName("Logger");
+
+  running_ = true;
+
+  while (running_) {
+    Update();
+    phased_loop.SleepUntilNext();
+  }
+}
+
+void Logger::Update() {
+  for (const auto& log : queue_logs_) {
+    std::experimental::optional<std::string> message;
+    while ((message = log->reader->GetMessageAsCSV())) {
+      writer_->WriteLine(log->filename, message.value());
+    }
+  }
+  for (const auto& log : text_logs_) {
+    std::experimental::optional<std::array<char, 1024>> message;
+    while ((message = log.queue->ReadMessage())) {
+      writer_->WriteLine(log.filename, std::string(&message.value()[0]));
+    }
+  }
+}
+
+void Logger::Start() { running_ = true; }
+
+void Logger::Stop() { running_ = false; }
+
+TextLogger Logger::MakeTextLogger(const std::string& name) {  // TODO(Wesley) logs with same name?
+  auto queue_ptr = std::make_shared<TextLogger::TextQueue>();
+  auto queue_reader = std::make_shared<TextLogger::TextQueue::QueueReader>(queue_ptr->MakeReader());
+  TextLog log_obj = {queue_reader, name, name + ".log"};
+  text_logs_.push_back(log_obj);
+  TextLogger logger(queue_ptr);
+  return logger;
+}
+
+}  // namespace logging
+}  // namespace muan
--- muan/logging/logger.hpp
+++ muan/logging/logger.hpp
@@ -1,12 +1,10 @@
 #ifndef MUAN_LOGGING_LOGGER_HPP_
 #define MUAN_LOGGING_LOGGER_HPP_
 
+#include "muan/logging/logger.h"
+
 namespace muan {
 namespace logging {
-
-Logger::Logger() { writer_ = std::make_unique<FileWriter>(); }
-
-Logger::Logger(std::unique_ptr<FileWriter>&& writer) : writer_(std::move(writer)) {}
 
 template <class T>
 void Logger::AddQueue(const std::string& name, T* queue_reader) {
@@ -18,48 +16,6 @@ void Logger::AddQueue(const std::string& name, T* queue_reader) {
   QueueLog queue_log = {std::make_unique<Reader<T>>(queue_reader), name, name + ".csv"};
 
   queue_logs_.push_back(std::make_unique<QueueLog>(std::move(queue_log)));
-}
-
-void Logger::Run() {
-  aos::time::PhasedLoop phased_loop(aos::time::Time::InMS(20));
-
-  aos::SetCurrentThreadRealtimePriority(20);
-  aos::SetCurrentThreadName("Logger");
-
-  running_ = true;
-
-  while (running_) {
-    Update();
-    phased_loop.SleepUntilNext();
-  }
-}
-
-void Logger::Update() {
-  for (const auto& log : queue_logs_) {
-    std::experimental::optional<std::string> message;
-    while ((message = log->reader->GetMessageAsCSV())) {
-      writer_->WriteLine(log->filename, message.value());
-    }
-  }
-  for (const auto& log : text_logs_) {
-    std::experimental::optional<std::array<char, 1024>> message;
-    while ((message = log.queue->ReadMessage())) {
-      writer_->WriteLine(log.filename, std::string(&message.value()[0]));
-    }
-  }
-}
-
-void Logger::Start() { running_ = true; }
-
-void Logger::Stop() { running_ = false; }
-
-TextLogger Logger::MakeTextLogger(const std::string& name) {  // TODO(Wesley) logs with same name?
-  auto queue_ptr = std::make_shared<TextLogger::TextQueue>();
-  auto queue_reader = std::make_shared<TextLogger::TextQueue::QueueReader>(queue_ptr->MakeReader());
-  TextLog log_obj = {queue_reader, name, name + ".log"};
-  text_logs_.push_back(log_obj);
-  TextLogger logger(queue_ptr);
-  return logger;
 }
 
 template <class T>
