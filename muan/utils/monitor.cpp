470ddf2f6d7d77791ff21bc807dcecbb4062b428
Finn Boire <finnegan.mccool@gmail.com>
2017-01-21 15:46:58 -0800

    Replace Motor Safety with Monitor
    * Abstract current-specific items to make it more portable
    * Add functionality for checking below a threshold or returning a custom voltage
    * Add more tests for the new functionalities added to Monitor

---
+++ muan/utils/monitor.cpp
@@ -0,0 +1,60 @@
+#include "monitor.h"
+
+namespace muan {
+
+namespace utils {
+
+Monitor::Monitor(double threshold, double delay_time, double reset_time, double dt, bool check_above, double standing_voltage)
+    : current_history_(dt) {
+  threshold_ = threshold;
+  delay_time_ = delay_time;
+  reset_time_ = reset_time;
+  dt_ = dt;
+  check_above_ = check_above;
+  standing_voltage_ = standing_voltage;
+
+  time_above_ = time_below_ = 0;
+  is_at_thresh_ = false;
+}
+
+double Monitor::Update(double voltage, double value) {
+  current_history_.Update(value);
+
+  // Take a moving average of the history array
+  double sum = 0;
+  std::array<double, kHistorySize> hist_arr = current_history_.get_hist_arr();
+  for (size_t i = 0; i < kHistorySize; i++) {
+    sum += hist_arr[i];
+  }
+  double moving_avg = sum / current_history_.num_samples();
+
+  // Determine if the value is above/below the threshold or not 
+  if ((moving_avg >= threshold_) == check_above_) {
+    time_above_ += dt_;
+    time_below_ = 0;
+  } else {
+    time_above_ = 0;
+    time_below_ += dt_;
+  }
+
+  // Determine if the motor has been stalling
+  if (time_above_ >= delay_time_) {
+    is_at_thresh_ = true;
+  } else if (time_below_ > reset_time_) {
+    is_at_thresh_ = false;
+  }
+
+  return is_at_thresh_ ? standing_voltage_ : voltage;
+}
+
+void Monitor::Reset() {
+  is_at_thresh_ = false;
+  time_above_ = 0;
+  time_below_ = 0;
+}
+
+bool Monitor::is_at_thresh() { return is_at_thresh_; }
+
+}
+
+}
