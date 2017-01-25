8ec68aeca6714122598776bc195134c3492ac58a
Wesley Aptekar-Cassels <W.Aptekar@gmail.com>
2017-01-25 00:01:06 -0800

    Add local path option to pull_logs script

--- scripts/pull_logs.py
+++ scripts/pull_logs.py
@@ -8,18 +8,21 @@ parser = argparse.ArgumentParser(description='Pull log files from the roboRIO')
 parser.add_argument('--team', default='1678', help='What team number to use for the roboRIO.')
 parser.add_argument('--address', help='What IP address to fetch from. Not compatible with --team')
 parser.add_argument('--user', default='admin', help='What user to log in as on the roboRIO')
-parser.add_argument('--path', default='/home/lvuser/logs/', help='What path to fetch logs from.')
-
+parser.add_argument('--remote-path', default='/home/lvuser/logs/', help='What path to fetch logs from.')
+parser.add_argument('--local-path', default=None, help="What path to write logs to. Defaults to logs/ in the repo's root")
 
 args = parser.parse_args()
 
-if not args.address:
+if args.address == None:
     rio_address = "roborio-{}-frc.local".format(args.team)
 else:
     rio_address = args.address
 rio_user = args.user
-rio_path = args.path
-local_path = os.path.abspath(os.path.dirname(__file__)) + "/../logs/" # Write to robot-code/logs/
+rio_path = args.remote_path
+if args.local_path == None:
+    local_path = os.path.abspath(os.path.dirname(__file__)) + "/../logs/" # Write to robot-code/logs/
+else:
+    local_path = args.local_path
 
 rsync_args = ["--verbose", "--archive", "--times", "--human-readable", "--progress"]
 rsync_remote_path = "{user}@{address}:{path}".format(user=rio_user, address=rio_address, path=rio_path)
