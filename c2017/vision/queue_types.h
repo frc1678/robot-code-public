e548fd334340ec6fe4236a56a5a82998fcbb9a7c
Lucas Solomon <amannababanana@gmail.com>
2017-01-28 09:12:45 -0800

    Add vision protos

---
+++ c2017/vision/queue_types.h
@@ -0,0 +1,23 @@
+#ifndef C2017_VISION_QUEUETYPES_H_
+#define C2017_VISION_QUEUETYPES_H_
+
+#include "muan/proto/stack_proto.h"
+#include "muan/queues/message_queue.h"
+#include "c2017/vision/vision.pb.h"
+
+namespace c2017 {
+namespace vision {
+
+using VisionInputProto = muan::proto::StackProto<VisionInput, 256>;
+using VisionStatusProto = muan::proto::StackProto<VisionStatus, 256>;
+using VisionGoalProto = muan::proto::StackProto<VisionGoal, 256>;
+
+using VisionInputQueue = muan::queues::MessageQueue<VisionInputProto, 100>;
+using VisionStatusQueue = muan::queues::MessageQueue<VisionStatusProto, 100>;
+using VisionGoalQueue = muan::queues::MessageQueue<VisionGoalProto, 100>;
+ 
+}  // namespace vision
+}  // namespace c2017
+
+#endif  // C2017_VISION_QUEUETYPES_H_
+
---
+++ c2017/vision/queue_types_test.cpp
@@ -0,0 +1,13 @@
+#include "queue_types.h"
+#include "gtest/gtest.h"
+
+TEST(Types, CanConstruct) {
+  c2017::vision::VisionInputProto input;
+  c2017::vision::VisionStatusProto status;
+  c2017::vision::VisionGoalProto goal;
+
+  c2017::vision::VisionInputQueue input_queue;
+  c2017::vision::VisionStatusQueue status_queue;
+  c2017::vision::VisionGoalQueue goal_queue;
+}
+
