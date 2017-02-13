f8895addf7d7d3bebb0a4de1b89dc5024865196a
Wesley Aptekar-Cassels <W.Aptekar@gmail.com>
2017-02-13 03:27:33 -0800

    Add trailing underscores to members of c2017 superstructure test fixture

--- c2017/subsystems/superstructure/superstructure_tests.cpp
+++ c2017/subsystems/superstructure/superstructure_tests.cpp
@@ -10,55 +10,55 @@ class SuperstructureTest : public ::testing::Test {
  public:
   SuperstructureTest() {}
 
-  intake_group::IntakeGroupGoalProto intake_group_goal_proto;
-  climber::ClimberGoalProto climber_goal_proto;
-  shooter_group::ShooterGroupGoalProto shooter_group_goal_proto;
+  intake_group::IntakeGroupGoalProto intake_group_goal_proto_;
+  climber::ClimberGoalProto climber_goal_proto_;
+  shooter_group::ShooterGroupGoalProto shooter_group_goal_proto_;
   muan::wpilib::DriverStationProto ds;
   superstructure::SuperStructure superstructure;
-  shooter::ShooterStatusProto shooter_status_proto;
+  shooter::ShooterStatusProto shooter_status_proto_;
 
-  climber::ClimberInputProto climber_input_proto;
-  shooter::ShooterInputProto shooter_input_proto;
-  ground_gear_intake::GroundGearIntakeInputProto ground_gear_input_proto;
-  magazine::MagazineInputProto magazine_input_proto;
+  climber::ClimberInputProto climber_input_proto_;
+  shooter::ShooterInputProto shooter_input_proto_;
+  ground_gear_intake::GroundGearIntakeInputProto ground_gear_input_proto_;
+  magazine::MagazineInputProto magazine_input_proto_;
 
   void WriteQueues() {
-    QueueManager::GetInstance().intake_group_goal_queue().WriteMessage(intake_group_goal_proto);
-    QueueManager::GetInstance().climber_goal_queue().WriteMessage(climber_goal_proto);
-    QueueManager::GetInstance().shooter_group_goal_queue().WriteMessage(shooter_group_goal_proto);
+    QueueManager::GetInstance().intake_group_goal_queue().WriteMessage(intake_group_goal_proto_);
+    QueueManager::GetInstance().climber_goal_queue().WriteMessage(climber_goal_proto_);
+    QueueManager::GetInstance().shooter_group_goal_queue().WriteMessage(shooter_group_goal_proto_);
     QueueManager::GetInstance().driver_station_queue()->WriteMessage(ds);
-    QueueManager::GetInstance().shooter_status_queue().WriteMessage(shooter_status_proto);
-    QueueManager::GetInstance().climber_input_queue().WriteMessage(climber_input_proto);
-    QueueManager::GetInstance().shooter_input_queue().WriteMessage(shooter_input_proto);
-    QueueManager::GetInstance().ground_gear_input_queue().WriteMessage(ground_gear_input_proto);
-    QueueManager::GetInstance().magazine_input_queue().WriteMessage(magazine_input_proto);
+    QueueManager::GetInstance().shooter_status_queue().WriteMessage(shooter_status_proto_);
+    QueueManager::GetInstance().climber_input_queue().WriteMessage(climber_input_proto_);
+    QueueManager::GetInstance().shooter_input_queue().WriteMessage(shooter_input_proto_);
+    QueueManager::GetInstance().ground_gear_input_queue().WriteMessage(ground_gear_input_proto_);
+    QueueManager::GetInstance().magazine_input_queue().WriteMessage(magazine_input_proto_);
   }
 
   void Reset() {
-    intake_group_goal_proto.Reset();
-    climber_goal_proto.Reset();
-    shooter_group_goal_proto.Reset();
+    intake_group_goal_proto_.Reset();
+    climber_goal_proto_.Reset();
+    shooter_group_goal_proto_.Reset();
     ds.Reset();
-    shooter_status_proto.Reset();
-    climber_input_proto.Reset();
-    shooter_input_proto.Reset();
-    ground_gear_input_proto.Reset();
-    magazine_input_proto.Reset();
+    shooter_status_proto_.Reset();
+    climber_input_proto_.Reset();
+    shooter_input_proto_.Reset();
+    ground_gear_input_proto_.Reset();
+    magazine_input_proto_.Reset();
   }
 };
 
 TEST_F(SuperstructureTest, GroundGearIntaking) {
-  climber_input_proto->set_current(0);
-  climber_input_proto->set_position(0);
-  shooter_input_proto->set_encoder_position(0);
-  ground_gear_input_proto->set_current(0);
-  magazine_input_proto->set_has_hp_gear(false);
+  climber_input_proto_->set_current(0);
+  climber_input_proto_->set_position(0);
+  shooter_input_proto_->set_encoder_position(0);
+  ground_gear_input_proto_->set_current(0);
+  magazine_input_proto_->set_has_hp_gear(false);
 
-  shooter_status_proto->set_at_goal(true);
+  shooter_status_proto_->set_at_goal(true);
 
   ds->set_mode(RobotMode::TELEOP);
-  intake_group_goal_proto->set_ground_intake_position(intake_group::INTAKE_GEAR);
-  intake_group_goal_proto->set_gear_intake(intake_group::GEAR_INTAKE);
+  intake_group_goal_proto_->set_ground_intake_position(intake_group::INTAKE_GEAR);
+  intake_group_goal_proto_->set_gear_intake(intake_group::GEAR_INTAKE);
 
   WriteQueues();
 
@@ -80,12 +80,12 @@ TEST_F(SuperstructureTest, GroundGearIntaking) {
 
 TEST_F(SuperstructureTest, GroundGearScoring) {
   ds->set_mode(RobotMode::TELEOP);
-  shooter_status_proto->set_at_goal(true);
+  shooter_status_proto_->set_at_goal(true);
 
-  intake_group_goal_proto->set_ground_intake_position(intake_group::INTAKE_NONE);
-  intake_group_goal_proto->set_hp_load_type(intake_group::HP_LOAD_NONE);
-  intake_group_goal_proto->set_roller(intake_group::ROLLERS_IDLE);
-  intake_group_goal_proto->set_gear_intake(intake_group::GEAR_OUTTAKE);
+  intake_group_goal_proto_->set_ground_intake_position(intake_group::INTAKE_NONE);
+  intake_group_goal_proto_->set_hp_load_type(intake_group::HP_LOAD_NONE);
+  intake_group_goal_proto_->set_roller(intake_group::ROLLERS_IDLE);
+  intake_group_goal_proto_->set_gear_intake(intake_group::GEAR_OUTTAKE);
 
   WriteQueues();
 
@@ -107,12 +107,12 @@ TEST_F(SuperstructureTest, GroundGearScoring) {
 
 TEST_F(SuperstructureTest, BallIntaking) {
   ds->set_mode(RobotMode::TELEOP);
-  shooter_status_proto->set_at_goal(true);
+  shooter_status_proto_->set_at_goal(true);
 
-  intake_group_goal_proto->set_ground_intake_position(intake_group::INTAKE_BALLS);
-  intake_group_goal_proto->set_hp_load_type(intake_group::HP_LOAD_NONE);
-  intake_group_goal_proto->set_roller(intake_group::ROLLERS_INTAKE);
-  intake_group_goal_proto->set_gear_intake(intake_group::GEAR_IDLE);
+  intake_group_goal_proto_->set_ground_intake_position(intake_group::INTAKE_BALLS);
+  intake_group_goal_proto_->set_hp_load_type(intake_group::HP_LOAD_NONE);
+  intake_group_goal_proto_->set_roller(intake_group::ROLLERS_INTAKE);
+  intake_group_goal_proto_->set_gear_intake(intake_group::GEAR_IDLE);
 
   WriteQueues();
 
@@ -135,13 +135,13 @@ TEST_F(SuperstructureTest, BallIntaking) {
 }
 
 TEST_F(SuperstructureTest, BallReverse) {
-  shooter_status_proto->set_at_goal(true);
+  shooter_status_proto_->set_at_goal(true);
 
   ds->set_mode(RobotMode::TELEOP);
-  intake_group_goal_proto->set_ground_intake_position(intake_group::INTAKE_BALLS);
-  intake_group_goal_proto->set_hp_load_type(intake_group::HP_LOAD_NONE);
-  intake_group_goal_proto->set_roller(intake_group::ROLLERS_OUTTAKE);
-  intake_group_goal_proto->set_gear_intake(intake_group::GEAR_IDLE);
+  intake_group_goal_proto_->set_ground_intake_position(intake_group::INTAKE_BALLS);
+  intake_group_goal_proto_->set_hp_load_type(intake_group::HP_LOAD_NONE);
+  intake_group_goal_proto_->set_roller(intake_group::ROLLERS_OUTTAKE);
+  intake_group_goal_proto_->set_gear_intake(intake_group::GEAR_IDLE);
 
   WriteQueues();
   superstructure.Update();
@@ -161,7 +161,7 @@ TEST_F(SuperstructureTest, BallReverse) {
 TEST_F(SuperstructureTest, ClimberTest) {
   ds->set_mode(RobotMode::TELEOP);
 
-  shooter_group_goal_proto->set_should_climb(true);
+  shooter_group_goal_proto_->set_should_climb(true);
 
   WriteQueues();
   superstructure.Update();
@@ -174,14 +174,14 @@ TEST_F(SuperstructureTest, ClimberTest) {
 
 TEST_F(SuperstructureTest, Shoot) {
   ds->set_mode(RobotMode::TELEOP);
-  shooter_status_proto->set_at_goal(true);
-  shooter_status_proto->set_currently_running(true);
-  intake_group_goal_proto->set_ground_intake_position(intake_group::INTAKE_BALLS);
-  intake_group_goal_proto->set_hp_load_type(intake_group::HP_LOAD_NONE);
-  intake_group_goal_proto->set_roller(intake_group::ROLLERS_INTAKE);
-  intake_group_goal_proto->set_gear_intake(intake_group::GEAR_IDLE);
-  shooter_group_goal_proto->set_wheel(shooter_group::SHOOT);
-  shooter_group_goal_proto->set_position(shooter_group::FENDER);
+  shooter_status_proto_->set_at_goal(true);
+  shooter_status_proto_->set_currently_running(true);
+  intake_group_goal_proto_->set_ground_intake_position(intake_group::INTAKE_BALLS);
+  intake_group_goal_proto_->set_hp_load_type(intake_group::HP_LOAD_NONE);
+  intake_group_goal_proto_->set_roller(intake_group::ROLLERS_INTAKE);
+  intake_group_goal_proto_->set_gear_intake(intake_group::GEAR_IDLE);
+  shooter_group_goal_proto_->set_wheel(shooter_group::SHOOT);
+  shooter_group_goal_proto_->set_position(shooter_group::FENDER);
 
   WriteQueues();
 
@@ -203,9 +203,9 @@ TEST_F(SuperstructureTest, Shoot) {
 
 TEST_F(SuperstructureTest, HPGearScoring) {
   ds->set_mode(RobotMode::TELEOP);
-  shooter_status_proto->set_at_goal(true);
+  shooter_status_proto_->set_at_goal(true);
 
-  intake_group_goal_proto->set_score_hp_gear(true);
+  intake_group_goal_proto_->set_score_hp_gear(true);
 
   WriteQueues();
 
