c958a27d522bf4b93a2dd9ea36fe8c322597a8e2
Lucas Solomon <amannababanana@gmail.com>
2017-02-19 13:36:11 -0800

    Comment jetson startup script

--- c2017/vision/coprocessor/startup.sh
+++ c2017/vision/coprocessor/startup.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
+#
+# This script runs vision and all the things vision needs:
+# * Turn on fan
+# * Set camera parameters - minimum exposure and brightness
+# * Run vision
+#
+# Usage: startup.sh roborio-ip
+#
+# To run this on startup, add the following line or something similar to /etc/rc.init
+#
+#     /home/ubuntu/robot-code/c2017/vision/coprocessor/startup.sh 10.16.78.22 >& /home/ubuntu/log.text &
+#
+
+# This script runs before the camera is ready, so sleep until it is ready.
 sleep 30s
 cd /home/ubuntu
 echo ubuntu | sudo -S ./jetson-scripts/jetson_max_l4t.sh
