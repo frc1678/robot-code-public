5907dd25ffdd303b8cdb926205404a745bfae46d
Lucas Solomon <amannababanana@gmail.com>
2017-02-20 11:04:02 -0800

    Use #if to enable vision logging

--- c2017/vision/coprocessor/vision.cpp
+++ c2017/vision/coprocessor/vision.cpp
@@ -5,6 +5,9 @@
 #include <iostream>
 #include "muan/vision/vision.h"
 
+#define VIDEO_OUTPUT_SCREEN 0
+#define VIDEO_OUTPUT_FILE 1
+
 namespace c2017 {
 namespace vision {
 
@@ -16,11 +19,13 @@ double VisionScorer2017::GetScore(double, double /* unused */, double skew, doub
 }
 
 void RunVision(int camera_index) {
-  // cv::VideoWriter output{"output.avi", CV_FOURCC('M', 'J', 'P', 'G'), 30, cv::Size(640 * 2, 480)};
+#if VIDEO_OUTPUT_FILE
+  cv::VideoWriter output{"output.avi", CV_FOURCC('M', 'J', 'P', 'G'), 30, cv::Size(640 * 2, 480)};
+#endif
   cv::VideoCapture cap;
   cap.open(camera_index);
   muan::Vision::ColorRange range{cv::Scalar(0, 50, 0), cv::Scalar(50, 180, 50), CV_BGR2RGB};
-  muan::Vision::VisionConstants constants{1.14, 0.599, 0.524, 1.66, 1};
+  muan::Vision::VisionConstants constants{1.14, 0.659, 0.524, 1.66, 1};
   muan::Vision vision{range, std::make_shared<VisionScorer2017>(), constants};
   cv::Mat raw;
   std::this_thread::sleep_for(std::chrono::milliseconds(1000));
@@ -34,14 +39,20 @@ void RunVision(int camera_index) {
       position->set_angle_to_target(status.angle_to_target + 0.134);
     }
     vision_queue.WriteMessage(position);
-    // cv::Mat splitscreen(status.image_canvas.rows, status.image_canvas.cols * 2, CV_8UC3);
-    // status.image_canvas.copyTo(splitscreen(cv::Rect(0, 0, status.image_canvas.cols,
-    // status.image_canvas.rows)));
-    // raw.copyTo(splitscreen(cv::Rect(status.image_canvas.cols, 0, status.image_canvas.cols,
-    // status.image_canvas.rows)));
-    // cv::imshow("vision", splitscreen);
-    // cv::waitKey(1);
-    // output.write(splitscreen);
+#if VIDEO_OUTPUT_FILE || VIDEO_OUTPUT_SCREEN
+    cv::Mat splitscreen(status.image_canvas.rows, status.image_canvas.cols * 2, CV_8UC3);
+    status.image_canvas.copyTo(splitscreen(cv::Rect(0, 0, status.image_canvas.cols,
+    status.image_canvas.rows)));
+    raw.copyTo(splitscreen(cv::Rect(status.image_canvas.cols, 0, status.image_canvas.cols,
+        status.image_canvas.rows)));
+#endif
+#if VIDEO_OUTPUT_SCREEN
+    cv::imshow("vision", splitscreen);
+    cv::waitKey(1);
+#endif
+#if VIDEO_OUTPUT
+    output.write(splitscreen);
+#endif
     status.image_canvas.release();
     raw.release();
   }
