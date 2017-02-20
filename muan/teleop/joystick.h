0183f7ffe02dfbe99048b2150ea80edf180a58bf
Wesley Aptekar-Cassels <W.Aptekar@gmail.com>
2017-02-20 13:29:48 -0800

    Revert Joystick API
    This changes the joystick API to use Make* instead of Get*.

--- muan/teleop/joystick.h
+++ muan/teleop/joystick.h
@@ -32,9 +32,9 @@ class Joystick {
 
   void Update();
 
-  muan::teleop::Button* GetButton(uint32_t button);
-  muan::teleop::Button* GetPov(uint32_t pov, Pov position);
-  muan::teleop::Button* GetAxis(uint32_t button);
+  muan::teleop::Button* MakeButton(uint32_t button);
+  muan::teleop::Button* MakePov(uint32_t pov, Pov position);
+  muan::teleop::Button* MakeAxis(uint32_t button);
 
   ::Joystick* wpilib_joystick();
 
@@ -44,8 +44,6 @@ class Joystick {
   JoystickStatusQueue* queue_;
 
   std::vector<std::unique_ptr<Button>> buttons_;
-  std::vector<std::unique_ptr<Button>> axis_;
-  std::vector<std::unique_ptr<Button>> pov_;
   ::Joystick wpilib_joystick_;
 };
 
