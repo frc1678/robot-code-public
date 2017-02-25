9ae41c7228f67c012d3be304867b7618baf59b7b
Kyle Stachowicz <kylestach99@gmail.com>
2017-02-25 09:47:40 -0800

    Fix issues in shooter controller
     - Typo in saturation protection
     - Controller feedforward initialization
     - Lower velocity tolerance to 30
     - Fix controller feedforward usage

--- c2017/subsystems/superstructure/shooter/shooter_controller.cpp
+++ c2017/subsystems/superstructure/shooter/shooter_controller.cpp
@@ -10,15 +10,19 @@ ShooterController::ShooterController()
   auto ss_plant = muan::control::StateSpacePlant<1, 3, 1>(frc1678::shooter_controller::controller::A(),
                                                           frc1678::shooter_controller::controller::B(),
                                                           frc1678::shooter_controller::controller::C());
-  controller_ = muan::control::StateSpaceController<1, 3, 1>(frc1678::shooter_controller::controller::K());
-  controller_.u_min() = Eigen::Matrix<double, 1, 1>::Ones() * -std::numeric_limits<double>::infinity();
-  controller_.u_max() = Eigen::Matrix<double, 1, 1>::Ones() * std::numeric_limits<double>::infinity();
+
+  controller_ = muan::control::StateSpaceController<1, 3, 1>(
+      frc1678::shooter_controller::controller::K(), frc1678::shooter_controller::controller::Kff(),
+      frc1678::shooter_controller::controller::A(),
+      Eigen::Matrix<double, 1, 1>::Ones() * -std::numeric_limits<double>::infinity(),
+      Eigen::Matrix<double, 1, 1>::Ones() * std::numeric_limits<double>::infinity());
+
   observer_ =
       muan::control::StateSpaceObserver<1, 3, 1>(ss_plant, frc1678::shooter_controller::controller::L());
 
   at_goal_ = false;
 
-  velocity_tolerance_ = 100;  // Radians per second
+  velocity_tolerance_ = 30;  // Radians per second
 }
 
 c2017::shooter::ShooterOutputProto ShooterController::Update(c2017::shooter::ShooterInputProto input,
@@ -29,9 +33,9 @@ c2017::shooter::ShooterOutputProto ShooterController::Update(c2017::shooter::Sho
   r_ = (Eigen::Matrix<double, 3, 1>() << 0.0, UpdateProfiledGoalVelocity(unprofiled_goal_velocity_), 0.0)
            .finished();
 
-  y(0) = (input->encoder_position());
+  controller_.r() = r_;
 
-  auto u = controller_.Update(observer_.x(), r_)(0, 0);
+  auto u = controller_.Update(observer_.x())(0, 0);
 
   if (!outputs_enabled || unprofiled_goal_velocity_ <= 0) {
     u = 0.0;
@@ -74,7 +78,7 @@ c2017::shooter::ShooterOutputProto ShooterController::Update(c2017::shooter::Sho
 double ShooterController::CapU(double u, bool outputs_enabled) {
   double k2 = controller_.K(0, 1);
   double x2 = observer_.x(1);
-  double k3 = controller_.K(0, 1);
+  double k3 = controller_.K(0, 2);
   double x3 = observer_.x(2);
 
   double u_max = 12;
