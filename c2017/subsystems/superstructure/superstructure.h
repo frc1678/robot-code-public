5523162d0cba73d36e1438bfb548baaefda8dcc0
LilTiger <tahoekeo@gmail.com>
2017-03-18 16:55:39 -0700

    Actually removing rumble_on_

--- c2017/subsystems/superstructure/superstructure.h
+++ c2017/subsystems/superstructure/superstructure.h
@@ -38,7 +38,6 @@ class SuperStructure {
   c2017::superstructure::SuperstructureStatusProto superstructure_status_proto_;
 
   bool is_shooting_ = false;
-  bool rumble_on_ = 0;
 
   void SetWpilibOutput();
 };
