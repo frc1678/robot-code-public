965ca3593445783009bfb7a6a9db047b2347e7b5
Wesley Aptekar-Cassels <W.Aptekar@gmail.com>
2017-03-27 13:32:19 -0700

    Make camera index by name script return index instead of filename

--- muan/vision/find_camera_by_name.py
+++ muan/vision/find_camera_by_name.py
@@ -4,7 +4,7 @@ import os
 import glob
 import argparse
 
-parser = argparse.ArgumentParser(description='Find a camera\' file in /dev by the name of the camera.')
+parser = argparse.ArgumentParser(description='Find a camera\'s index in by the name of the camera.')
 parser.add_argument('--name', required=True, help='A substring of the camera name that you want.')
 args = parser.parse_args()
 
@@ -12,4 +12,4 @@ for camera in glob.glob("/sys/class/video4linux/video*/"):
     with open(os.path.join(camera, "name")) as camera_file:
         camera_name = camera_file.read()
         if args.name in camera_name:
-            print(os.path.join("/dev/", os.path.basename(os.path.normpath(camera))))
+            print(os.path.basename(os.path.normpath(camera))[-1])
