0b25be8f207f965f11e5eb1886ee67800395bc12
Wesley Aptekar-Cassels <W.Aptekar@gmail.com>
2017-03-28 09:20:45 -0700

    Add camera params for stream camera

--- c2017/vision/coprocessor/startup.sh
+++ c2017/vision/coprocessor/startup.sh
@@ -22,7 +22,8 @@ echo Robot ip: $1
 echo Vision camera index: $VISION_CAM
 echo Stream camera index: $STREAM_CAM
 
-v4l2ctrl -d /dev/video$VISION_CAM -l c2017/vision/coprocessor/camera_params
+v4l2ctrl -d /dev/video$VISION_CAM -l c2017/vision/coprocessor/vision_camera_params
+v4l2ctrl -d /dev/video$STREAM_CAM -l c2017/vision/coprocessor/stream_camera_params
 
-mjpg_streamer -i "input_uvc.so -d /dev/video$STREAM_CAM" -o "output_http.so" &
+mjpg_streamer -i "input_uvc.so -d /dev/video$STREAM_CAM" -o "output_http.so -p 5802" &
 ./bazel-bin/muan/autostart/autostart ./bazel-bin/c2017/vision/coprocessor/main --robot_ip=$1 --camera_index=$VISION_CAM
