6b62b3a347b5ad6782287b755a847984149d9a41
Kyle Stachowicz <kylestach99@gmail.com>
2017-03-28 22:46:45 -0700

    Make ground gear intake constants public

--- c2017/subsystems/superstructure/ground_gear_intake/ground_gear_intake.h
+++ c2017/subsystems/superstructure/ground_gear_intake/ground_gear_intake.h
@@ -9,6 +9,13 @@ namespace c2017 {
 
 namespace ground_gear_intake {
 
+constexpr double kIntakeVoltage = 12.0;
+constexpr double kPickupVoltage = 2.5;
+constexpr double kCarryVoltage = 1.5;
+constexpr double kScoreVoltage = -12.0;
+constexpr int kPickupTicks = 300;
+constexpr double kCurrentThreshold = 60.0;
+
 class GroundGearIntake {
  public:
   GroundGearIntake() = default;
@@ -18,13 +25,6 @@ class GroundGearIntake {
   State current_state() const;
 
  private:
-  static constexpr double kIntakeVoltage = 12.0;
-  static constexpr double kPickupVoltage = 2.5;
-  static constexpr double kCarryVoltage = 1.5;
-  static constexpr double kScoreVoltage = -12.0;
-  static constexpr int kPickupTicks = 300;
-  static constexpr double kCurrentThreshold = 60.0;
-
   State current_state_ = IDLE;
   int pickup_timer_ = 0;
 };
