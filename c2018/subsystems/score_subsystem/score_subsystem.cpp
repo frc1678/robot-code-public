971a20eb9500f3e242106aa827ad2293d3ac57bc
ZachCal <zach_inator@att.net>
2018-01-15 11:05:15 -0800

    Refined calculations and removed lines with no purpose.

--- c2018/subsystems/score_subsystem/score_subsystem.cpp
+++ c2018/subsystems/score_subsystem/score_subsystem.cpp
@@ -20,13 +20,12 @@ void ScoreSubsystem::Update() {
   }
 
   if (!goal->god_mode()) {
-    goal->set_god_mode(false);
     if (!status->elevator_at_top()) {
       claw_mode_ = SCORE_F;
     } else {
       claw_mode_ = goal->claw_mode();
       if (claw_mode_ == SCORE_F) {
-        claw_angle = 0;
+        claw_angle = M_PI/2;
       } else {
         claw_angle = -1*(M_PI/2);
       }
@@ -34,14 +33,10 @@ void ScoreSubsystem::Update() {
     intake_mode_ = goal->intake_mode();
     elevator_height = goal->elevator_height();
   } else {
-    goal->set_god_mode(true);
     claw_mode_ = SCORE_F;
-    goal->set_elevator_velocity(goal->elevator_velocity());
-    goal->set_intake_voltage(goal->intake_voltage());
   }
   claw_.SetGoal(claw_angle, intake_mode_);
   elevator_.SetGoal(elevator_height);
-  elevator_.SetGodModeGoal();
 }
 
 }  // namespace score_subsystem
