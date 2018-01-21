f183df64e3267452d55ebd017eb1e226b49f2b67
Zach Callahan <zach_inator@att.net>
2018-01-21 14:06:17 -0800

    Add input queue to score subsystem test

--- c2018/subsystems/score_subsystem/score_subsystem_test.cpp
+++ c2018/subsystems/score_subsystem/score_subsystem_test.cpp
@@ -8,9 +8,12 @@ class ScoreSubsystemTest : public ::testing::Test {
   void Update() { score_subsystem_.Update(); }
 
   void ReadMessages() {
-    
 
-  }
+
  private:
   c2018::score_subsystem::ScoreSubsystem score_subsystem_;
+  muan::queues::MessageQueue<c2018::score_subsystem::ScoreSubsystemInputProto>* input_queue_
+  c2018::score_subsystem::ScoreSubsystemGoalQueue* score_subsystem_qoal_queue_ =
+  		muan::queues::QueueManager<c2018::score_subsystem::ScoreSubsystemGoalProto>::Fetch();
+  c2018::score_subsystem::ScoreSubsystemInputQueue
 };
