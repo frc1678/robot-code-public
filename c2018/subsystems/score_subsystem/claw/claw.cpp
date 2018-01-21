e234b94a4756cf99ca1d077479a38988b80361a5
Apple61752 <mohamedapple@hotmail.com>
2018-01-20 16:12:10 -0800

    Claw Mechanism
    *Add a python file for constant generation
    *No tests made yet
    *Make a controller for the mechanism
    	*intake
    	*wrist

---
+++ c2018/subsystems/score_subsystem/claw/claw.cpp
@@ -0,0 +1,162 @@
+#include "c2018/subsystems/score_subsystem/claw/claw.h"
+#include <cmath>
+
+namespace c2018 {
+namespace score_subsystem {
+namespace claw {
+
+ClawController::ClawController()
+    : trapezoidal_motion_profile_{::std::chrono::milliseconds(5)},
+      hall_calibration_{kHallMagnetPosition} {
+  auto claw_plant = muan::control::StateSpacePlant<1, 3, 1>(
+      frc1678::claw_controller::controller::A(),
+      frc1678::claw_controller::controller::B(),
+      frc1678::claw_controller::controller::C());
+
+  claw_controller_ = muan::control::StateSpaceController<1, 3, 1>(
+      frc1678::claw_controller::controller::K(),
+      frc1678::claw_controller::controller::Kff(),
+      frc1678::claw_controller::controller::A(),
+      Eigen::Matrix<double, 1, 1>::Ones() * -12,
+      Eigen::Matrix<double, 1, 1>::Ones() * 12);
+
+  claw_observer_ = muan::control::StateSpaceObserver<1, 3, 1>(
+      claw_plant, frc1678::claw_controller::controller::L());
+
+  trapezoidal_motion_profile_.set_maximum_acceleration(kMaxClawAcceleration);
+  trapezoidal_motion_profile_.set_maximum_velocity(kMaxClawVelocity);
+}
+
+void ClawController::SetGoal(double angle, IntakeMode mode) {
+  if (claw_state_ == C_IDLE || claw_state_ == C_MOVING) {
+    unprofiled_goal_position_ = muan::utils::Cap(angle, 0, M_PI);
+    claw_state_ = C_MOVING;
+  }
+
+  intake_mode_ = mode;
+}
+
+void ClawController::Update(ScoreSubsystemInputProto input,
+                            ScoreSubsystemOutput* output,
+                            ScoreSubsystemStatusProto* status,
+                            bool outputs_enabled) {
+  double calibrated_encoder =
+      hall_calibration_.Update(input->wrist_encoder(), input->wrist_hall());
+  auto claw_y =
+      (Eigen::Matrix<double, 1, 1>() << calibrated_encoder).finished();
+
+  double claw_voltage = 0;
+
+  if (!outputs_enabled) {
+    claw_voltage = 0;
+    claw_state_ = C_DISABLED;
+  } else if (claw_state_ == C_DISABLED) {
+    claw_state_ = C_IDLE;
+  } else if (!encoder_fault_detected_) {
+    claw_voltage = CapU(claw_voltage, outputs_enabled);
+  }
+
+  if (claw_state_ == C_INITIALIZING) {
+    claw_state_ = C_CALIBRATING;
+  } else if (claw_state_ == C_CALIBRATING) {
+    claw_voltage = kCalibVoltage;
+    if (hall_calibration_.is_calibrated()) {
+      claw_state_ = C_IDLE;
+    }
+  } else if (claw_state_ == C_IDLE || claw_state_ == C_MOVING) {
+    // Run the controller
+    Eigen::Matrix<double, 3, 1> claw_r;
+    claw_r = (Eigen::Matrix<double, 3, 1>() << UpdateProfiledGoal(unprofiled_goal_position_, outputs_enabled)).finished();
+    claw_r.block<2, 1>(0, 0) =
+        trapezoidal_motion_profile_.Update(unprofiled_goal_position_, 0.0);
+    claw_r(2) = 0;
+
+    claw_voltage = claw_controller_.Update(claw_observer_.x(), claw_r)(0);
+  }
+
+  // Check for encoder faults
+  if (old_pos_ == input->wrist_encoder()) {
+    num_encoder_fault_ticks_++;
+    if (num_encoder_fault_ticks_ > kEncoderFaultTicksAllowed) {
+      encoder_fault_detected_ = true;
+    }
+  }
+  old_pos_ = input->wrist_encoder();
+
+  claw_observer_.Update(
+      (Eigen::Matrix<double, 1, 1>() << claw_voltage).finished(), claw_y);
+
+  // Start of intake
+  double roller_voltage = 0;
+  bool claw_pinch = false;
+
+  if (outputs_enabled) {
+    switch (intake_mode_) {
+      case INTAKE:
+        roller_voltage = 12;
+        claw_pinch = true;
+        break;
+      case OUTAKE:  // TODO (Mohamed) fix spelling of outtake once Zach does
+        roller_voltage = -12;
+        claw_pinch = true;
+        break;
+      case IDLE:
+        roller_voltage = 0;
+        claw_pinch = false;
+        break;
+      case HOLD:
+        roller_voltage = 0;
+        claw_pinch = true;
+        break;
+    }
+  } else {
+    roller_voltage = 0;
+    claw_pinch = false;
+  }
+
+  output->set_roller_voltage(roller_voltage);
+  output->set_wrist_voltage(claw_voltage);
+  output->set_claw_pinch(claw_pinch);
+  (*status)->set_wrist_position(wrist_position_);
+}
+
+double ClawController::CapU(double claw_voltage, bool outputs_enabled) {
+  double voltage;
+  if (!outputs_enabled) {
+    voltage = 0;
+  } else if (claw_voltage > 12) {
+    voltage = 12;
+  } else if (claw_voltage < -12) {
+    voltage = -12;
+  } else {
+    voltage = claw_voltage;
+  }
+
+  return voltage;
+}
+
+double ClawController::CapGoal(double r) {
+// cap goal between 0 and pi
+double goal = unprofiled_goal_position_;
+
+if (r > M_PI) {
+  goal = M_PI;
+} else if (r < 0){
+  goal = 0;
+} else {
+  goal = unprofiled_goal_position_;
+}
+
+return goal;
+}
+
+Eigen::Matrix<double, 2, 1> ClawController::UpdateProfiledGoal(double unprofiled_goal_, bool outputs_enabled) {
+    if (outputs_enabled) {
+      profiled_goal_ = trapezoidal_motion_profile_.Update(unprofiled_goal_, 0.0);
+    }
+
+    return profiled_goal_;
+}
+}  // namespace claw
+}  // namespace score_subsystem
+}  // namespace c2018
---
+++ c2018/subsystems/score_subsystem/score_subsystem.h
@@ -0,0 +1,41 @@
+#ifndef C2018_SUBSYSTEMS_SCORE_SUBSYSTEM_SCORE_SUBSYSTEM_H_
+#define C2018_SUBSYSTEMS_SCORE_SUBSYSTEM_SCORE_SUBSYSTEM_H_
+
+#include <cmath>
+#include "c2018/subsystems/score_subsystem/claw/claw.h"
+#include "c2018/subsystems/score_subsystem/queue_types.h"
+#include "muan/queues/queue_manager.h"
+#include "muan/wpilib/queue_types.h"
+
+namespace c2018 {
+
+namespace score_subsystem {
+
+class ScoreSubsystem {
+ public:
+  ScoreSubsystem();
+  void Update();
+
+ private:
+  frc1678::ClawController claw_;
+  c2018::score_subsystem::elevator::Elevator elevator_;
+  c2018::score_subsystem::ScoreSubsystemGoalQueue::QueueReader goal_reader_;
+  c2018::score_subsystem::ScoreSubsystemStatusQueue::QueueReader status_reader_;
+  c2018::score_subsystem::ScoreSubsystemGoalQueue score_subsystem_goal_queue_;
+  c2018::score_subsystem::ScoreSubsystemGoalProto score_subsystem_goal_proto_;
+  c2018::score_subsystem::ScoreSubsystemInputQueue::QueueReader input_reader_;
+  c2018::score_subsystem::ScoreSubsystemOutputQueue* output_queue_;
+  c2018::score_subsystem::ScoreSubsystemInputQueue* input_queue_;
+  muan::wpilib::DriverStationQueue::QueueReader ds_status_;
+
+  double elevator_height;
+  double claw_angle;
+  IntakeMode intake_mode_;
+  ClawMode claw_mode_;
+};
+
+}  // namespace score_subsystem
+
+}  // namespace c2018
+
+#endif  // C2018_SUBSYSTEMS_SCORE_SUBSYSTEM_SCORE_SUBSYSTEM_H_
