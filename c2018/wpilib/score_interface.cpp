a1f078f1841b12d029e935a2c6d6803c5ae45da9
hansonl02 <32380392+hansonl02@users.noreply.github.com>
2018-01-27 14:27:56 -0800

    Update constants and math logic in c2018/wpilib/score_interface.cpp (#493)
    * Rename citrus_robot to teleop and removed auto references from main.cpp
    
    * Clang format and save all files because I forgot to last commit
    
    * Rename namespace and class and fix c2018/BUILD as Kyle requested
    
    * Rename places where names were still old to fix errors
    
    * Fix c2018/main.cpp
    
    * Linting
    
    * Update constants and math logic
    
    * Make ints doubles
    
    * Fix math

--- c2018/wpilib/score_interface.cpp
+++ c2018/wpilib/score_interface.cpp
@@ -7,6 +7,11 @@
 namespace c2018 {
 namespace wpilib {
 
+constexpr double kPitchRadius = (1. + (1. / 16.)) * 0.0254;
+
+constexpr double kElevatorSensorRatio = 2.14;
+constexpr double kWristSensorRatio = 5.14;
+
 constexpr uint32_t kElevatorMotor = 4;
 constexpr uint32_t kIntakeMotor = 2;
 constexpr uint32_t kWristMotor = 3;
@@ -50,8 +55,10 @@ ScoreSubsystemInterface::ScoreSubsystemInterface(
 
 void ScoreSubsystemInterface::ReadSensors() {
   ScoreSubsystemInputProto sensors;
-  sensors->set_elevator_encoder(elevator_encoder_.Get() / 2.2222);
-  sensors->set_wrist_encoder(wrist_encoder_.Get() / 5.14);
+  sensors->set_elevator_encoder(elevator_encoder_.Get() * kPitchRadius *
+                                (2 * M_PI) / 512 / kElevatorSensorRatio);
+  sensors->set_wrist_encoder(wrist_encoder_.Get() * (2 * M_PI) / 512 /
+                             kWristSensorRatio);
   // These numbers come from the status to outpur ratios for the encoders.
   sensors->set_elevator_hall(elevator_hall_.Get());
   sensors->set_wrist_hall(wrist_hall_.Get());
