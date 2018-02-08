2270f0c416ca9cbefbf25c7277712958b7bb45a8
S-Mackenzie1678 <32380353+S-Mackenzie1678@users.noreply.github.com>
2018-02-07 20:23:19 -0800

    Motor Calibration 2 (#496)
    * Merge master
    
    * Fix broken calibration code
    
    * Comment blank comment to fix Git's weirdness
    
    * fix indent error on line 22
    
    * Fix cpplint errors
    
    * Fix indentation errors
    
    * Initialize calibrate_start_ and give it a button (click the right stick in on the XBOX controller)
    
    * Assign button the proper way (the button is West o Pov)
    
    * Fix format error in BUILD file and correctly make the Pov
    The button to hold to run this is West on the Pov
    
    * Fix transitive include statement
    
    * fix location of `button_maker_->Update()`

--- testbench/motor_calibration/motor_calibration.cpp
+++ testbench/motor_calibration/motor_calibration.cpp
@@ -1,23 +1,28 @@
-#include <WPILib.h>
 #include <chrono>
 #include <iostream>
 #include <memory>
 #include <thread>
 
+#include "WPILib.h"
+#include "muan/teleop/button.h"
+#include "muan/teleop/joystick.h"
+
 class MotorCalibration : public IterativeRobot {
  public:
-  MotorCalibration() {
-    int port_to_be_calibrated;
+  muan::teleop::Joystick* button_maker_;
+  muan::teleop::Button* calibrate_start_ =
+      button_maker_->MakePov(0, muan::teleop::Pov::kWest);
+  int port_to_be_calibrated;
 
+  MotorCalibration() {
     std::cout << "Which port do you want to be calibrated?" << std::endl;
     std::cin >> port_to_be_calibrated;
-
     motor_ = std::make_unique<VictorSP>(port_to_be_calibrated);
-    controller_ = std::make_unique<GenericHID>(0);
   }
 
   void TeleopPeriodic() override {
-    if (controller_->GetRawButton(0 /* This might need to be 1 */)) {
+    button_maker_->Update();
+    if (calibrate_start_->is_pressed()) {
       motor_->Set(1.0);
       std::this_thread::sleep_for(std::chrono::seconds(2));
       motor_->Set(-1.0);
@@ -26,8 +31,7 @@ class MotorCalibration : public IterativeRobot {
   }
 
  private:
-  VictorSP* motor_;
-  GenericHID controller_;
+  std::unique_ptr<VictorSP> motor_;
 };
 
 START_ROBOT_CLASS(MotorCalibration);
