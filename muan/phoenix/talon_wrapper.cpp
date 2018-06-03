61e6635a051dd29eca09c76110478731f64733f8
Jishnu Sen <jishnusen@users.noreply.github.com>
2018-06-03 14:40:35 -0700

    Add wrappers for Speedcontrollers from Phoenix lib (#632)
    * Add simple wrapper for TalonSRX
    
    * Add SP factory to easily make default config talons and be sure about them
    
    * No more talon pointers
    
    * Move structs inside the class to get rid of repetetive naming
    
    * Lint
    
    * Add load config function to easily change config later
    
    * Add talon output units
    
    * Add output units and cleanup
    
    * Move talon output units into talon_wrapper
    
    * Rename ramping rate to ramping time
    
    * Add units to arbitrary feed-forwards
    
    * Remove unneeded conditional
    
    * Add method to follow another talon
    
    * Feedback sensor in Config
    
    * Set feedback sensor for both slots
    
    * Add talon GetID function
    
    * Make openloop goal in volts (assumes 12V bus)
    
    * Remove useless `;`
    
    * Linting
    
    * Add max_voltage, and voltage compensation

---
+++ muan/phoenix/talon_wrapper.cpp
@@ -0,0 +1,148 @@
+#include "muan/phoenix/talon_wrapper.h"
+#include "muan/units/units.h"
+
+namespace muan {
+namespace phoenix {
+
+TalonWrapper::TalonWrapper(int id, Config config)
+    : talon_(id), conversion_factor_(config.conversion_factor) {
+  LoadConfig(config);
+}
+
+void TalonWrapper::LoadConfig(Config config) {
+  config_ = config;
+  talon_.Set(ControlMode::PercentOutput, 0.);  // Safety first!
+
+  switch (config.sensor) {
+    case FeedbackSensor::kMagEncoderRelative:
+      talon_.ConfigSelectedFeedbackSensor(
+          FeedbackDevice::CTRE_MagEncoder_Relative, 0, kTalonSetupTimeout);
+      talon_.ConfigSelectedFeedbackSensor(
+          FeedbackDevice::CTRE_MagEncoder_Relative, 1, kTalonSetupTimeout);
+      break;
+    case FeedbackSensor::kMagEncoderAbsolute:
+      talon_.ConfigSelectedFeedbackSensor(
+          FeedbackDevice::CTRE_MagEncoder_Absolute, 0, kTalonSetupTimeout);
+      talon_.ConfigSelectedFeedbackSensor(
+          FeedbackDevice::CTRE_MagEncoder_Absolute, 1, kTalonSetupTimeout);
+      break;
+    case FeedbackSensor::kNone:
+      break;
+  }
+
+  talon_.ChangeMotionControlFramePeriod(config.motion_control_frame_period);
+  talon_.ClearMotionProfileHasUnderrun(kTalonSetupTimeout);
+  talon_.ClearMotionProfileTrajectories();
+
+  talon_.ClearStickyFaults(kTalonSetupTimeout);
+
+  talon_.ConfigForwardLimitSwitchSource(LimitSwitchSource_FeedbackConnector,
+                                        LimitSwitchNormal_NormallyOpen,
+                                        kTalonSetupTimeout);
+  talon_.ConfigReverseLimitSwitchSource(LimitSwitchSource_FeedbackConnector,
+                                        LimitSwitchNormal_NormallyOpen,
+                                        kTalonSetupTimeout);
+  talon_.OverrideLimitSwitchesEnable(config.enable_limit_switch);
+
+  talon_.ConfigSetParameter(ParamEnum::eClearPositionOnLimitF, 0, 0, 0,
+                            kTalonSetupTimeout);
+  talon_.ConfigSetParameter(ParamEnum::eClearPositionOnLimitR, 0, 0, 0,
+                            kTalonSetupTimeout);
+
+  talon_.ConfigNominalOutputForward(0, kTalonSetupTimeout);
+  talon_.ConfigNominalOutputReverse(0, kTalonSetupTimeout);
+  talon_.ConfigNeutralDeadband(config.neutral_deadband, kTalonSetupTimeout);
+
+  talon_.ConfigPeakOutputForward(1., kTalonSetupTimeout);
+  talon_.ConfigPeakOutputReverse(-1., kTalonSetupTimeout);
+
+  talon_.SetNeutralMode(config.neutral_mode);
+
+  talon_.ConfigForwardSoftLimitThreshold(
+      config.forward_soft_limit * conversion_factor_, kTalonSetupTimeout);
+  talon_.ConfigForwardSoftLimitEnable(config.enable_soft_limit,
+                                      kTalonSetupTimeout);
+
+  talon_.ConfigReverseSoftLimitThreshold(
+      config.reverse_soft_limit * conversion_factor_, kTalonSetupTimeout);
+  talon_.ConfigReverseSoftLimitEnable(config.enable_soft_limit,
+                                      kTalonSetupTimeout);
+
+  talon_.OverrideSoftLimitsEnable(config.enable_soft_limit);
+
+  talon_.SetInverted(config.motor_inverted);
+  talon_.SetSensorPhase(config.sensor_inverted);
+
+  talon_.SelectProfileSlot(0, 0);
+
+  talon_.ConfigVelocityMeasurementPeriod(config.velocity_measurement_period,
+                                         kTalonSetupTimeout);
+  talon_.ConfigVelocityMeasurementWindow(config.velocity_measurement_window,
+                                         kTalonSetupTimeout);
+
+  talon_.ConfigOpenloopRamp(config.open_loop_ramp_time, kTalonSetupTimeout);
+  talon_.ConfigClosedloopRamp(config.closed_loop_ramp_time, kTalonSetupTimeout);
+
+  talon_.ConfigVoltageCompSaturation(config.max_voltage, kTalonSetupTimeout);
+  talon_.ConfigVoltageMeasurementFilter(32, kTalonSetupTimeout);
+  talon_.EnableVoltageCompensation(config.compensate_voltage);
+
+  talon_.SetStatusFramePeriod(StatusFrameEnhanced::Status_1_General,
+                              config.general_status_frame_rate,
+                              kTalonSetupTimeout);
+  talon_.SetStatusFramePeriod(StatusFrameEnhanced::Status_2_Feedback0,
+                              config.feedback_status_frame_rate,
+                              kTalonSetupTimeout);
+  talon_.SetStatusFramePeriod(StatusFrameEnhanced::Status_3_Quadrature,
+                              config.quadrature_status_frame_rate,
+                              kTalonSetupTimeout);
+  talon_.SetStatusFramePeriod(StatusFrameEnhanced::Status_4_AinTempVbat,
+                              config.analog_temp_vbat_status_frame_rate,
+                              kTalonSetupTimeout);
+  talon_.SetStatusFramePeriod(StatusFrameEnhanced::Status_8_PulseWidth,
+                              config.pwm_status_frame_rate, kTalonSetupTimeout);
+
+  talon_.SetControlFramePeriod(ControlFrame::Control_3_General,
+                               config.control_frame_period);
+}
+
+void TalonWrapper::SetOpenloopGoal(double setpoint) {  // Voltage
+  talon_.Set(ControlMode::PercentOutput, setpoint / 12.);
+}
+
+void TalonWrapper::SetPositionGoal(double setpoint,
+                                   double setpoint_ff) {  // Position, Voltage
+  talon_.Set(ControlMode::Position, setpoint * conversion_factor_,
+             DemandType_ArbitraryFeedForward, setpoint_ff * kTalonOutput);
+}
+
+void TalonWrapper::SetVelocityGoal(double setpoint,
+                                   double setpoint_ff) {  // Velocity, Voltage
+  talon_.Set(ControlMode::Velocity, setpoint * conversion_factor_,
+             DemandType_ArbitraryFeedForward, setpoint_ff * kTalonOutput);
+}
+
+void TalonWrapper::SetGains(Gains gains, int slot) {
+  talon_.Config_kP(slot, gains.p * kTalonOutput / conversion_factor_,
+                   kTalonSetupTimeout);
+  talon_.Config_kI(
+      slot, gains.i * kTalonOutput / conversion_factor_ * muan::units::ms,
+      kTalonSetupTimeout);
+  talon_.Config_kD(
+      slot, gains.d * kTalonOutput / conversion_factor_ / muan::units::ms,
+      kTalonSetupTimeout);
+  talon_.Config_kF(slot, gains.f * kTalonOutput / conversion_factor_,
+                   kTalonSetupTimeout);
+
+  talon_.ConfigMaxIntegralAccumulator(slot, gains.max_integral,
+                                      kTalonSetupTimeout);
+  talon_.Config_IntegralZone(slot, gains.i_zone, kTalonSetupTimeout);
+
+  talon_.ConfigAllowableClosedloopError(slot, gains.deadband,
+                                        kTalonSetupTimeout);
+}
+
+void TalonWrapper::SelectGains(int slot) { talon_.SelectProfileSlot(slot, 0); }
+
+}  // namespace phoenix
+}  // namespace muan
---
+++ muan/phoenix/talon_wrapper.h
@@ -0,0 +1,107 @@
+#ifndef MUAN_PHOENIX_TALON_WRAPPER_H_
+#define MUAN_PHOENIX_TALON_WRAPPER_H_
+
+#include "ctre/Phoenix.h"
+
+namespace muan {
+namespace phoenix {
+
+constexpr int kTalonSetupTimeout = 100;
+constexpr int kTalonRegularTimeout = 10;
+constexpr int kTalonOutput = 1023. / 12.;  // Per volt
+
+enum class FeedbackSensor {
+  kMagEncoderRelative,
+  kMagEncoderAbsolute,
+  kNone,
+};
+
+class TalonWrapper {
+ public:
+  struct Gains {  // SI Units, mechanism specific
+    double p;
+    double i;
+    double d;
+    double f;
+    double i_zone;
+    double max_integral;
+    double deadband;
+  };
+
+  struct Config {
+    // Neutral
+    NeutralMode neutral_mode = NeutralMode::Coast;
+    double neutral_deadband = 0.04;
+
+    // Limits
+    bool enable_current_limit = false;
+    bool enable_soft_limit = false;
+    bool enable_limit_switch = false;
+    int forward_soft_limit = 0;
+    int reverse_soft_limit = 0;
+
+    bool motor_inverted = false;
+    bool sensor_inverted = false;
+
+    // Frame periods (ms)
+    int control_frame_period = 5;
+    int motion_control_frame_period = 100;
+    int general_status_frame_rate = 5;
+    int feedback_status_frame_rate = 5;
+    int quadrature_status_frame_rate = 5;
+    int analog_temp_vbat_status_frame_rate = 5;
+    int pwm_status_frame_rate = 5;
+
+    // Velocity Measurement
+    VelocityMeasPeriod velocity_measurement_period =
+        VelocityMeasPeriod::Period_100Ms;
+    int velocity_measurement_window = 32;  // samples / period
+
+    // Ramp rates
+    double open_loop_ramp_time = 0.;    // seconds to 12V
+    double closed_loop_ramp_time = 0.;  // seconds to 12V
+
+    double conversion_factor = 1.;  // native / actual
+    FeedbackSensor sensor = FeedbackSensor::kNone;
+
+    bool compensate_voltage = true;
+    double max_voltage = 12.;
+  };
+
+  TalonWrapper(int id, Config config);  // Specified config
+  void LoadConfig(Config config);
+
+  // Set talon output
+  void SetFollower(int id) { talon_.Set(ControlMode::Follower, id); }
+  void SetOpenloopGoal(double setpoint);
+  void SetPositionGoal(double setpoint, double setpoint_ff);
+  void SetVelocityGoal(double setpoint, double setpoint_ff);
+
+  // PID gains for a given slot
+  void SetGains(Gains gains, int slot);
+  void SelectGains(int slot);
+
+  void ResetSensor(double value = 0) {
+    talon_.SetSelectedSensorPosition(value, 0, kTalonRegularTimeout);
+  }
+
+  // Getters
+  inline int id() { return talon_.GetDeviceID(); }
+  inline Config config() { return config_; }
+  inline double position() { return talon_.GetSelectedSensorPosition(0); }
+  inline double velocity() { return talon_.GetSelectedSensorVelocity(0); }
+  inline double voltage() { return talon_.GetMotorOutputVoltage(); }
+  inline double percent() { return talon_.GetMotorOutputPercent(); }
+  inline double current() { return talon_.GetOutputCurrent(); }
+
+ private:
+  TalonSRX talon_;
+  Config config_;
+
+  double conversion_factor_;  // native / actual
+};
+
+}  // namespace phoenix
+}  // namespace muan
+
+#endif  //  MUAN_PHOENIX_TALON_WRAPPER_H_
